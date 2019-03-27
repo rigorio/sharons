a61e0d0f431422dee7af0bec57d3becaba2f79e2
rigo_sarmiento <rigo.sarmiento@pm..me>
2019-03-27 12:14:41 +0800

    Initial commit

---
+++ src/main/java/inc/pabacus/TaskMetrics/TaskMetricsApplication.java
@@ -0,0 +1,13 @@
+package inc.pabacus.TaskMetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMetricsApplication.class, args);
+	}
+
+}

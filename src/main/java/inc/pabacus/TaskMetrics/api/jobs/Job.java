30cf54024a89aa035df332c10354212614c1a265
rigorio <RigoS@pabacus.com>
2019-03-29 19:58:18 +0800

    Job details

---
+++ src/main/java/inc/pabacus/TaskMetrics/api/jobs/Job.java
@@ -0,0 +1,30 @@
+package inc.pabacus.TaskMetrics.api.jobs;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Job {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String title;
+  private String description;
+  private String dateCreated;
+  private String progress;
+  private String assignee;
+  private String author;
+
+}

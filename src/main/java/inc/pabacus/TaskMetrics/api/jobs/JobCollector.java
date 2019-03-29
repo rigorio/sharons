627423ac9758f0a33d574d9972e9d0890e52ebce
rigorio <RigoS@pabacus.com>
2019-03-29 20:01:52 +0800

    Jobs can now be fetched

---
+++ src/main/java/inc/pabacus/TaskMetrics/api/jobs/JobCollector.java
@@ -0,0 +1,32 @@
+package inc.pabacus.TaskMetrics.api.jobs;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class JobCollector {
+
+  private JobRepository jobRepository;
+
+  public JobCollector(JobRepository jobRepository) {
+    this.jobRepository = jobRepository;
+  }
+
+  public Optional<Job> getJob(Long id) {
+    return jobRepository.findById(id);
+  }
+
+  public List<Job> getAllJobs() {
+    return jobRepository.findAll();
+  }
+
+  public List<Job> searchJobs(String keyword) {
+    return jobRepository.findAll().stream()
+        .filter(job -> job.getTitle().toLowerCase().contains(keyword.toLowerCase()) ||
+            job.getDescription().toLowerCase().contains(keyword.toLowerCase()))
+        .collect(Collectors.toList());
+  }
+
+
+
+}

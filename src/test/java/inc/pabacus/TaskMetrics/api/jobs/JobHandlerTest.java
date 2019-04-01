3e439fa2e68da9a6fb79547d5f4045d7b429e6b3
rigorio <RigoS@pabacus.com>
2019-04-01 11:30:35 +0800

    Moved mock to higher scope; filtered by status

--- src/test/java/inc/pabacus/TaskMetrics/api/jobs/JobHandlerTest.java
+++ src/test/java/inc/pabacus/TaskMetrics/api/jobs/JobHandlerTest.java
@@ -1,12 +1,11 @@
 package inc.pabacus.TaskMetrics.api.jobs;
 
-import inc.pabacus.TaskMetrics.api.jobs.options.Progress;
 import inc.pabacus.TaskMetrics.api.jobs.options.Status;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -16,41 +15,34 @@ public class JobHandlerTest extends MockJobDataProvider {
 
   private JobService jobService;
   private JobRepository jobRepository;
+  private List<Job> list = getJobs();
 
   @Before
   public void init() {
     jobRepository = mock(JobRepository.class);
     jobService = new JobHandler(jobRepository);
+    when(jobRepository.findAll()).thenReturn(list);
   }
 
   @Test
   public void testFindAll() {
-    List<Job> list = new ArrayList<>();
-    Job job = new Job(1L, "Job 1", "Description",
-                      Status.BACKLOG, Progress.SEVENTY_FIVE,
-                      "10/20/19", "Rigo Sarmiento");
-    list.add(job);
-
-    when(jobRepository.findAll()).thenReturn(list);
     List<Job> allJobs = jobService.getAllJobs();
-
     assertEquals(list, allJobs);
   }
 
   @Test
   public void testSearchWithKeyword() {
-    List<Job> list = getJobs();
-
-    when(jobRepository.findAll()).thenReturn(list);
-
     List<Job> jobs = jobService.searchJobs("keyword");
     list.removeIf(job -> job.getId().equals(0L));
-
     assertEquals(list, jobs);
   }
 
   @Test
   public void testFilterByStatus() {
-
+    List<Job> jobs = jobService.searchJobs(Status.BACKLOG);
+    List<Job> backLogJobs = getJobs().stream()
+        .filter(job -> job.getStatus().equals(Status.BACKLOG))
+        .collect(Collectors.toList());
+    assertEquals(backLogJobs, jobs);
   }
 }

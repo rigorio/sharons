9ec54b490c83ba78c726d9adfb2733d6205cfc9a
rigorio <RigoS@pabacus.com>
2019-04-01 09:39:38 +0800

    Initial testing for service and repo

--- src/test/java/inc/pabacus/TaskMetrics/api/jobs/JobCollectorTest.java
+++ src/test/java/inc/pabacus/TaskMetrics/api/jobs/JobCollectorTest.java
@@ -1,17 +1,39 @@
 package inc.pabacus.TaskMetrics.api.jobs;
 
+import inc.pabacus.TaskMetrics.api.jobs.options.Progress;
+import inc.pabacus.TaskMetrics.api.jobs.options.Status;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class JobCollectorTest {
 
   private JobService jobService;
+  private JobRepository jobRepository;
 
   @Before
   public void init() {
-    JobRepository jobRepository = mock(JobRepository.class);
+    jobRepository = mock(JobRepository.class);
     jobService = new JobCollector(jobRepository);
   }
 
+  @Test
+  public void testFindAll() {
+    List<Job> list = new ArrayList<>();
+    Job job = new Job(1L, "Job 1", "Description",
+                      Status.BACKLOG, Progress.SEVENTY_FIVE,
+                      "10/20/19", "Rigo Sarmiento");
+    list.add(job);
+
+    when(jobRepository.findAll()).thenReturn(list);
+    List<Job> allJobs = jobService.getAllJobs();
+
+    assertEquals(list, allJobs);
+  }
 }

a9e4963da5864a1d5760a05e5329f2db4ddce8c9
rigorio <RigoS@pabacus.com>
2019-04-02 09:58:43 +0800

    Fixed test to simulate an event to the listener

--- src/test/java/inc/pabacus/TaskMetrics/api/listener/ActivityListenerTest.java
+++ src/test/java/inc/pabacus/TaskMetrics/api/listener/ActivityListenerTest.java
@@ -31,8 +31,16 @@ public class ActivityListenerTest {
     activityListener.setEvent(event);
     activityListener.setInterval(100); // override default 5 minute interval
     activityListener.listen();
-    Thread.sleep(150L);
-    assertEquals(1, i);
+
+    // the following events will send a mouse event to the listener
+    Thread.sleep(50L);
+    activityListener.invokeEvent();
+    Thread.sleep(50L);
+    activityListener.invokeEvent();
+    Thread.sleep(50L);
+    activityListener.invokeEvent();
+
+    assertEquals(0, i); // Event should not be executed
   }
 
   @Test

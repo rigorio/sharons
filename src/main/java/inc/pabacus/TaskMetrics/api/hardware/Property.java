34311d15fc331ba4ef52e813aed8ff8663db4365
rigorio <RigoS@pabacus.com>
2019-04-03 20:51:49 +0800

    Add all arguments constructor

--- src/main/java/inc/pabacus/TaskMetrics/api/hardware/Property.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/hardware/Property.java
@@ -1,8 +1,10 @@
 package inc.pabacus.TaskMetrics.api.hardware;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Property {
   private String title;
   private String spec;

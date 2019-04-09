99ca567af60c33d752b5828e86ac8e7ba83f27c3
rigorio <RigoS@pabacus.com>
2019-04-09 11:31:14 +0800

    Better usage for hardware service

--- src/main/java/inc/pabacus/TaskMetrics/api/hardware/HardwareData.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/hardware/HardwareData.java
@@ -1,31 +1,8 @@
 package inc.pabacus.TaskMetrics.api.hardware;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public interface HardwareData {
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+  String getName();
+  String getDescription();
 
-@Data
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-public class HardwareData {
-
-  @Id
-  @GeneratedValue
-  private Long id;
-  private String name;
-  private String type;
-  private String serialNumber;
-  private String manufacturer;
-
-  public HardwareData(String name, String type, String serialNumber, String manufacturer) {
-    this.name = name;
-    this.type = type;
-    this.serialNumber = serialNumber;
-    this.manufacturer = manufacturer;
-  }
 }

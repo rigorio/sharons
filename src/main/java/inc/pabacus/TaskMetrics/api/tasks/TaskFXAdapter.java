a23c2b05374515b553449eb66c3998475aa6d388
rigorio <RigoS@pabacus.com>
2019-04-09 15:40:01 +0800

    Updated adapter for new fields

--- src/main/java/inc/pabacus/TaskMetrics/api/tasks/TaskFXAdapter.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/TaskFXAdapter.java
@@ -3,12 +3,17 @@ package inc.pabacus.TaskMetrics.api.tasks;
 import javafx.beans.property.*;
 import lombok.Data;
 
+/**
+ * TODO add the other fields to the constructor adapter?
+ */
 @Data
 public class TaskFXAdapter {
 
   private LongProperty id;
   private StringProperty title;
   private StringProperty description;
+  private IntegerProperty priority;
+  private StringProperty totalTimeSpent;
   private StringProperty status;
   private IntegerProperty progress;
   private StringProperty dateCreated;
@@ -19,6 +24,8 @@ public class TaskFXAdapter {
     id = taskId != null ? new SimpleLongProperty(taskId) : null;
     title = new SimpleStringProperty(task.getTitle());
     description = new SimpleStringProperty(task.getDescription());
+    priority = new SimpleIntegerProperty(task.getProgress().getProgress());
+    totalTimeSpent = new SimpleStringProperty(task.getTotalTimeSpent());
     status = new SimpleStringProperty(task.getStatus().getStatus());
     progress = new SimpleIntegerProperty(task.getProgress().getProgress());
     dateCreated = new SimpleStringProperty(task.getDateCreated());

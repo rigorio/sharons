79cc1b0c271877dfdc4d38e198976b936581cd31
rigorio <RigoS@pabacus.com>
2019-04-10 07:31:50 +0800

    Added two tables for timesheet and tasksheet

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/timesheet/TimesheetPresenter.java
@@ -0,0 +1,21 @@
+package inc.pabacus.TaskMetrics.desktop.timesheet;
+
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TimesheetPresenter implements Initializable {
+
+  @FXML
+  private JFXTreeTableView taskSheet;
+  @FXML
+  private JFXTreeTableView timeSheet;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}

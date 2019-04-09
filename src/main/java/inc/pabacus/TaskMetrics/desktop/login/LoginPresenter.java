85d5e2251b33bb9a69f1783c160a5f23c6adb52e
rigorio <RigoS@pabacus.com>
2019-04-09 12:16:17 +0800

    Beautifying login

--- src/main/java/inc/pabacus/TaskMetrics/desktop/login/LoginPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/login/LoginPresenter.java
@@ -1,5 +1,8 @@
 package inc.pabacus.TaskMetrics.desktop.login;
 
+import inc.pabacus.TaskMetrics.desktop.dashboard.DashboardView;
+import inc.pabacus.TaskMetrics.utils.GuiManager;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
@@ -9,5 +12,10 @@ public class LoginPresenter implements Initializable {
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
 
+  }
+
+  @FXML
+  public void login() {
+    GuiManager.getInstance().showView(new DashboardView());
   }
 }

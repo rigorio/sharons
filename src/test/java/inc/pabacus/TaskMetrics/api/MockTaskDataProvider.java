7052dbeda44321046a54ef3a68589f048846b39a
rigorio <RigoS@pabacus.com>
2019-04-11 17:18:02 +0800

    Temporarily removed inapplicable mock data

--- src/test/java/inc/pabacus/TaskMetrics/api/MockTaskDataProvider.java
+++ src/test/java/inc/pabacus/TaskMetrics/api/MockTaskDataProvider.java
@@ -11,18 +11,18 @@ public class MockTaskDataProvider {
 
   public List<Task> getJobs() {
     List<Task> list = new ArrayList<>();
-    list.add(new Task(1L, "KeyWordss", "Description",
-                      Status.BACKLOG, Progress.FIFTY,
-                      "10/20/19", "Rigo Sarmiento"));
-    list.add(new Task(1L, "Task 1", "aaa keywoRD",
-                      Status.IN_PROGRESS, Progress.ONE_HUNDRED,
-                      "10/20/19", "Rigo Sarmiento"));
-    list.add(new Task(1L, "Task 1", "Description",
-                      Status.BACKLOG, Progress.SEVENTY_FIVE,
-                      "10/20/19", "ASDF asdfKEYWORD asdf"));
-    list.add(new Task(0L, "Task 1", "Description",
-                      Status.FOR_REVIEW, Progress.FIFTY,
-                      "10/20/19", "Rigo Sarmiento"));
+//    list.add(new Task(1L, "KeyWordss", "Description",
+//                      Status.BACKLOG, Progress.FIFTY,
+//                      "10/20/19", "Rigo Sarmiento"));
+//    list.add(new Task(1L, "Task 1", "aaa keywoRD",
+//                      Status.IN_PROGRESS, Progress.ONE_HUNDRED,
+//                      "10/20/19", "Rigo Sarmiento"));
+//    list.add(new Task(1L, "Task 1", "Description",
+//                      Status.BACKLOG, Progress.SEVENTY_FIVE,
+//                      "10/20/19", "ASDF asdfKEYWORD asdf"));
+//    list.add(new Task(0L, "Task 1", "Description",
+//                      Status.FOR_REVIEW, Progress.FIFTY,
+//                      "10/20/19", "Rigo Sarmiento"));
     return list;
   }
 

ff70b818f1bbb94c9e27a79cba10ac07340f8d7c
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-04-12 09:52:51 +0800

    Application icon

--- src/main/java/inc/pabacus/TaskMetrics/TaskMetricsApplication.java
+++ src/main/java/inc/pabacus/TaskMetrics/TaskMetricsApplication.java
@@ -3,6 +3,7 @@ package inc.pabacus.TaskMetrics;
 import inc.pabacus.TaskMetrics.desktop.login.LoginView;
 import inc.pabacus.TaskMetrics.utils.GuiManager;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,6 +21,7 @@ public class TaskMetricsApplication extends Application {
   @Override
   public void start(Stage stage) {
     stage.setResizable(false);
+    stage.getIcons().add(new Image("/img/PabacusLogo.png"));
     MANAGER.setPrimaryStage(stage);
     MANAGER.showView(new LoginView());
   }

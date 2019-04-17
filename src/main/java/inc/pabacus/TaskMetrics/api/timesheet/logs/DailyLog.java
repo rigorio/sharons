1184f8cf4cbda18b31b1e78e7e5370fca88bd635
Rigo Sarmiento <RigoS@pabacus.com>
2019-04-17 10:25:49 +0800

    Converted to JPA Entity

--- src/main/java/inc/pabacus/TaskMetrics/api/timesheet/logs/DailyLog.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/timesheet/logs/DailyLog.java
@@ -4,15 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 public class DailyLog {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
   private String date;
   private String in;
   private String otl;
   private String bfl;
   private String out;
 
+  public DailyLog(String date, String in, String otl, String bfl, String out) {
+    this.date = date;
+    this.in = in;
+    this.otl = otl;
+    this.bfl = bfl;
+    this.out = out;
+  }
 }

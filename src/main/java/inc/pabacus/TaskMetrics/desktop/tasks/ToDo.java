b24aeaaa3df4d49cca6d61102436ba3aab7bed64
Rigo Sarmiento <RigoS@pabacus.com>
2019-04-17 10:25:06 +0800

    Added right fields

--- src/main/java/inc/pabacus/TaskMetrics/desktop/tasks/ToDo.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/tasks/ToDo.java
@@ -1,27 +1,75 @@
 package inc.pabacus.TaskMetrics.desktop.tasks;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class ToDo {
-    protected SimpleStringProperty toDo;
 
-    public ToDo(String toDo) {
-        this.toDo = new SimpleStringProperty(toDo);
-    }
+  private StringProperty title;
+  private StringProperty description;
+  private StringProperty status;
+  private StringProperty timeSpent;
 
-    public String getToDo() {
-        return toDo.get();
-    }
+  public ToDo(String title, String description, String status) {
+    this.title = new SimpleStringProperty(title);
+    this.description = new SimpleStringProperty(description);
+    this.status = new SimpleStringProperty(status);
+  }
 
-    public SimpleStringProperty toDoProperty() {
-        return toDo;
-    }
+  public ToDo(String title, String description, String status, String timeSpent) {
+    this.title = new SimpleStringProperty(title);
+    this.description = new SimpleStringProperty(description);
+    this.status = new SimpleStringProperty(status);
+    this.timeSpent = new SimpleStringProperty(timeSpent);
+  }
 
-    public void setToDo(String toDo) {
-        this.toDo.set(toDo);
-    }
+  public String getTimeSpent() {
+    return timeSpent.get();
+  }
 
-    public ToDo() {
-    }
+  public StringProperty timeSpentProperty() {
+    return timeSpent;
+  }
+
+  public void setTimeSpent(String timeSpent) {
+    this.timeSpent.set(timeSpent);
+  }
+
+  public String getTitle() {
+    return title.get();
+  }
+
+  public StringProperty titleProperty() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title.set(title);
+  }
+
+  public String getDescription() {
+    return description.get();
+  }
+
+  public StringProperty descriptionProperty() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description.set(description);
+  }
+
+  public StringProperty getStatus() {
+    return status;
+  }
+
+  public String statusProperty() {
+    return status.get();
+  }
+
+  public void setStatus(String status) {
+    this.status.set(status);
+  }
+
 }
 

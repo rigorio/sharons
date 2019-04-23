5de1d99998a1636d25d3b0f83bae6ad6080668c5
Rigo Sarmiento <RigoS@pabacus.com>
2019-04-23 20:58:08 +0800

    Added converter for string to enum

--- src/main/java/inc/pabacus/TaskMetrics/api/tasks/options/Status.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/options/Status.java
@@ -1,16 +1,31 @@
 package inc.pabacus.TaskMetrics.api.tasks.options;
 
-public enum Status implements Option{
+public enum Status implements Option {
 
   BACKLOG("Backlog"),
   IN_PROGRESS("In Progress"),
   FOR_REVIEW("For Review"),
-  DONE("Closed");
+  DONE("Done");
 
   private String status;
 
   Status(String status) {
     this.status = status;
+  }
+
+  public static Status convert(String s) {
+    switch (s) {
+      case "Backlog":
+        return BACKLOG;
+      case "In Progress":
+        return IN_PROGRESS;
+      case "For review":
+        return FOR_REVIEW;
+      case "Done":
+        return DONE;
+      default:
+        return null;
+    }
   }
 
   public String getStatus() {

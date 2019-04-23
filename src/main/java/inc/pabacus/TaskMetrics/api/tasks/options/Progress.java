5788ac7fc73fcadfbd17009f268edb6720fc7b8e
Rigo Sarmiento <RigoS@pabacus.com>
2019-04-23 20:57:19 +0800

    Converter for integer to enum

--- src/main/java/inc/pabacus/TaskMetrics/api/tasks/options/Progress.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/options/Progress.java
@@ -1,6 +1,6 @@
 package inc.pabacus.TaskMetrics.api.tasks.options;
 
-public enum Progress implements Option{
+public enum Progress implements Option {
 
   ZERO(0),
   TWENTY_FIVE(25),
@@ -16,5 +16,22 @@ public enum Progress implements Option{
 
   public Integer getProgress() {
     return progress;
+  }
+
+  public static Progress convert(Integer progress) {
+    switch (progress) {
+      case 0:
+        return ZERO;
+      case 25:
+        return TWENTY_FIVE;
+      case 50:
+        return FIFTY;
+      case 75:
+        return SEVENTY_FIVE;
+      case 100:
+        return ONE_HUNDRED;
+      default:
+        return ZERO;
+    }
   }
 }

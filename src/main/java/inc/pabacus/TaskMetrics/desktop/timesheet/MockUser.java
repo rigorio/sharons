095f54935701e66ee4bcb14ff500018c161efac8
Rigo Sarmiento <RigoS@pabacus.com>
2019-04-24 12:18:15 +0800

    Mock data for status and name

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/timesheet/MockUser.java
@@ -0,0 +1,11 @@
+package inc.pabacus.TaskMetrics.desktop.timesheet;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MockUser {
+  private String name;
+  private String status;
+}

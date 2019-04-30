188e0955848e81527c920883adcf63e7f5ac390d
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-01 07:47:57 +0800

    Undecorated, draggable display always on top

--- src/main/java/inc/pabacus/TaskMetrics/utils/GuiManager.java
+++ src/main/java/inc/pabacus/TaskMetrics/utils/GuiManager.java
@@ -3,6 +3,7 @@ package inc.pabacus.TaskMetrics.utils;
 import com.airhacks.afterburner.views.FXMLView;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class GuiManager {
 
@@ -29,6 +30,31 @@ public class GuiManager {
   public void displayView(FXMLView view) {
     Stage stage = new Stage();
     stage.setScene(new Scene(view.getView()));
+    stage.show();
+  }
+
+  private double xOffset = 0;
+  private double yOffset = 0;
+
+  public void displayAlwaysOnTop(FXMLView view) {
+    Stage stage = new Stage();
+
+    Scene scene = new Scene(view.getView());
+    scene.setOnMousePressed(event -> {
+      xOffset = event.getSceneX();
+      yOffset = event.getSceneY();
+    });
+
+    //move around here
+    scene.setOnMouseDragged(event -> {
+      stage.setX(event.getScreenX() - xOffset);
+      stage.setY(event.getScreenY() - yOffset);
+    });
+
+    stage.initStyle(StageStyle.UNDECORATED);
+    stage.setScene(scene);
+    stage.setAlwaysOnTop(true);
+    stage.setResizable(false);
     stage.show();
   }
 

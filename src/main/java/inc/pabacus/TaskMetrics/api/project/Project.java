d5aabbc117a4986254552a2251ed63af240d14ba
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-02 13:09:33 +0800

    Adapter constructors

--- src/main/java/inc/pabacus/TaskMetrics/api/project/Project.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/project/Project.java
@@ -1,5 +1,8 @@
 package inc.pabacus.TaskMetrics.api.project;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.StringProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +24,33 @@ public class Project {
   private Double invoiceAmount;
   private List<ProjectTask> projectTasks;
 
+  public Project(ProjectFXAdapter project) {
+
+    LongProperty id = project.getId();
+    this.id = id != null ? id.get() : null;
+
+    StringProperty projectName = project.getProjectName();
+    this.projectName = projectName != null ? projectName.get() : null;
+
+    DoubleProperty billable = project.getBillable();
+    this.billable = billable != null ? billable.get() : null;
+
+    DoubleProperty nonBillable = project.getNonBillable();
+    this.nonBillable = nonBillable != null ? nonBillable.get() : null;
+
+    DoubleProperty totalHours = project.getTotalHours();
+    this.totalHours = totalHours != null ? totalHours.get() : null;
+
+    DoubleProperty percentBillable = project.getPercentBillable();
+    this.percentBillable = percentBillable != null ? percentBillable.get() : null;
+
+    DoubleProperty billRate = project.getBillRate();
+    this.billRate = billRate != null ? billRate.get() : null;
+
+    DoubleProperty invoiceAmount = project.getInvoiceAmount();
+    this.invoiceAmount = invoiceAmount != null ? invoiceAmount.get() : null;
+
+    this.projectTasks = project.getProjectTasks();
+
+  }
 }

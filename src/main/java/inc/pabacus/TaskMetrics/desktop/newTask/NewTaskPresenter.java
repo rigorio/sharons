ef8651a109feb0fbda52aa00b77ec12b0477491e
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-06 13:53:06 +0800

    Conflicts again

--- src/main/java/inc/pabacus/TaskMetrics/desktop/newTask/NewTaskPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/newTask/NewTaskPresenter.java
@@ -1,156 +1,148 @@
 package inc.pabacus.TaskMetrics.desktop.newTask;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import inc.pabacus.TaskMetrics.api.project.Project;
+import inc.pabacus.TaskMetrics.api.project.ProjectHandler;
+import inc.pabacus.TaskMetrics.api.tasks.Task;
+import inc.pabacus.TaskMetrics.api.tasks.TaskHandler;
+import inc.pabacus.TaskMetrics.api.tasks.businessValue.BusinessValue;
+import inc.pabacus.TaskMetrics.api.tasks.businessValue.BusinessValueHandler;
+import inc.pabacus.TaskMetrics.api.tasks.options.Status;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 import okhttp3.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class NewTaskPresenter implements Initializable {
 
-    @FXML
-    private JFXTextField descriptionField;
+  @FXML
+  private JFXTextField descriptionField;
 
-    @FXML
-    private JFXComboBox<String> billableComboBox;
+  @FXML
+  private JFXComboBox<String> billableComboBox;
 
-    @FXML
-    private JFXButton saveButton;
+  @FXML
+  private JFXButton saveButton;
 
-    @FXML
-    private JFXButton closeButton;
+  @FXML
+  private JFXButton closeButton;
 
-    @FXML
-    private JFXComboBox<String> projectComboBox;
+  @FXML
+  private JFXComboBox<String> projectComboBox;
 
-    @FXML
-    private JFXComboBox<String> businessComboBox;
+  @FXML
+  private JFXComboBox<String> businessComboBox;
 
-    ObservableList<String> projectList = FXCollections.observableArrayList("ABC","XYZ","ASD");
-    ObservableList<String> billableList = FXCollections.observableArrayList("True","False");
+  ObservableList<String> billableList = FXCollections.observableArrayList("True", "False");
 
-    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    private static final String HOST = "http://localhost:8080";
+  private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+  private static final String HOST = "http://localhost:8080";
+  private BusinessValueHandler businessValueHandler = new BusinessValueHandler();
+  private ProjectHandler projectHandler = new ProjectHandler();
+  private TaskHandler taskHandler = new TaskHandler();
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
 
-    getItems();
+    //unfocus textfield
+    Platform.runLater(() -> closeButton.requestFocus()); // what
+
+    List<String> businesses = getAllBusinessValues().stream()
+        .map(BusinessValue::getBusiness)
+        .collect(Collectors.toList());
+    businessComboBox.getItems().addAll(businesses);
     billableComboBox.setPromptText("Choose Billable");
     projectComboBox.setPromptText("Choose Project");
     businessComboBox.setPromptText("Choose Business Value");
-    projectComboBox.setItems(projectList);
+    List<String> projects = getAllProjects().stream()
+        .map(Project::getProjectName)
+        .collect(Collectors.toList());
+    projectComboBox.setItems(FXCollections.observableArrayList(projects));
     billableComboBox.setItems(billableList);
-    // set default value
-    projectComboBox.setValue("ABC");
-    billableComboBox.setValue("True");
-    businessComboBox.setValue("Accounting");
+  }
+
+  @FXML
+  public void close() {
+    Stage stage = (Stage) projectComboBox.getScene().getWindow();
+    stage.close();
+  }
+
+  @FXML
+  public void save() {
+    boolean isMprojectComboBoxEmpty = projectComboBox.getSelectionModel().isEmpty();
+    boolean isbillableComboBoxEmpty = billableComboBox.getSelectionModel().isEmpty();
+    boolean isbusinessComboBoxEmpty = businessComboBox.getSelectionModel().isEmpty();
+    boolean isDescriptionEmpty = descriptionField.getText().isEmpty();
+
+    if (isMprojectComboBoxEmpty || isbillableComboBoxEmpty || isDescriptionEmpty || isbusinessComboBoxEmpty) {
+      Alert alert = new Alert(Alert.AlertType.WARNING);
+      alert.setTitle("Error");
+      alert.setContentText("Please fill out all the fields");
+      alert.showAndWait();
+    } else {
+      String title = descriptionField.getText(); // actually title of task
+      Boolean billable = Boolean.valueOf(billableComboBox.getValue());
+
+      BusinessValue businessValue = getBusinessValue();
+      Project project = getProject();
+
+      Task task = Task.builder()
+          .title(title)
+          .projectId(project.getId())
+          .projectName(project.getProjectName())
+          .businessValueId(businessValue.getId())
+          .billable(billable)
+          .timeSpent(0.0)
+          .status(Status.BACKLOG)
+          .build();
+
+      Task t = taskHandler.createTask(task);
+      Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setContentText("Task saved!");
+      alert.showAndWait();
+
     }
+  }
 
-    @FXML
-    public void close(){
-        Stage stage = (Stage) projectComboBox.getScene().getWindow();
-        stage.close();
-    }
+  private Project getProject() {
+    Optional<Project> anyProject = getAllProjects().stream()
+        .filter(project -> project.getProjectName().equals(projectComboBox.getValue()))
+        .findAny();
 
-    @FXML
-    public void save(){
-        boolean isMprojectComboBoxEmpty = projectComboBox.getSelectionModel().isEmpty();
-        boolean isbillableComboBoxEmpty = billableComboBox.getSelectionModel().isEmpty();
-        boolean isbusinessComboBoxEmpty = businessComboBox.getSelectionModel().isEmpty();
-        boolean isDescriptionEmpty = descriptionField.getText().isEmpty();
+    if (!anyProject.isPresent())
+      throw new RuntimeException("No Project was found");
 
-        if(isMprojectComboBoxEmpty || isbillableComboBoxEmpty || isDescriptionEmpty || isbusinessComboBoxEmpty){
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Error");
-            alert.setContentText("Please fill out all the fields");
-            alert.showAndWait();
-        }else{
-            String title = projectComboBox.getValue();
-            String description = descriptionField.getText();
-            Boolean billable = Boolean.valueOf(billableComboBox.getValue());
-            String status = "BACKLOG";
-            NewTaskPresenter insertTask = new NewTaskPresenter();
-            InsertTask task = insertTask.saveTask(new InsertTask(title,description,billable,status));
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle(null);
-            alert.setHeaderText(null);
-            alert.setContentText("Task saved!");
-            alert.showAndWait();
-            //close the window after creating the task
-            Stage stage = (Stage) projectComboBox.getScene().getWindow();
-            stage.close();
-        }
-    }
+    return anyProject.get();
+  }
 
-    public InsertTask saveTask(InsertTask task) {
-        InsertTask insertTask;
-        try {
-            OkHttpClient client = new OkHttpClient();
-            ObjectMapper mapper = new ObjectMapper();
+  private BusinessValue getBusinessValue() {
+    Optional<BusinessValue> any = getAllBusinessValues().stream()
+        .filter(businessValue -> businessValue.getBusiness().equals(businessComboBox.getValue()))
+        .findAny();
 
-            String jsonString = mapper.writeValueAsString(task);
-            RequestBody body = RequestBody.create(JSON, jsonString);
+    if (!any.isPresent())
+      throw new RuntimeException("No business value found"); // throw dialog box
 
-            Call call = client.newCall(new Request.Builder()
-                    .url(HOST + "/api/task") // TODO add path
-                    .post(body)
-                    .build());
-            ResponseBody responseBody = call.execute().body();
-            insertTask = mapper.readValue(responseBody.string(),
-                    new TypeReference<InsertTask>() {});
-            return insertTask;
-        } catch (IOException e) {
-            return task;
-        }
-    }
+    return any.get();
+  }
 
-    public void getItems(){
-        try {
-            OkHttpClient client = new OkHttpClient();
-            // code request code here
-            Request request = new Request.Builder()
-                    .url("http://localhost:8080/api/businessValue")
-                    .addHeader("Accept", "application/json")
-                    .method("GET", null)
-                    .build();
+  private List<BusinessValue> getAllBusinessValues() {
+    return businessValueHandler.getAll();
+  }
 
-            Response response = null;
-
-            response = client.newCall(request).execute();
-
-            String getTimes = response.body().string();
-
-            JSONArray jsonArray = new JSONArray(getTimes);
-
-            for(int i=0;i<jsonArray.length();i++)
-            {
-                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
-                String value1 = jsonObject1.getString("business");
-
-                businessComboBox.getItems().addAll(value1);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
+  private List<Project> getAllProjects() {
+    return projectHandler.getAllProjects();
+  }
 }

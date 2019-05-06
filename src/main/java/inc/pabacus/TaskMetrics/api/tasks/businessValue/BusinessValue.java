00194670b15b3b2868b2aaf50ed2d793afb0aa33
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-06 13:11:01 +0800

    Business Value API

---
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/businessValue/BusinessValue.java
@@ -0,0 +1,10 @@
+package inc.pabacus.TaskMetrics.api.tasks.businessValue;
+
+import lombok.Data;
+
+@Data
+public class BusinessValue {
+  private Long id;
+  private String business;
+  private Double value;
+}
---
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/businessValue/BusinessValueHandler.java
@@ -0,0 +1,38 @@
+package inc.pabacus.TaskMetrics.api.tasks.businessValue;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessValueHandler {
+
+  private OkHttpClient client = new OkHttpClient();
+  private ObjectMapper mapper = new ObjectMapper();
+  private static final String HOST = "http://localhost:8080";
+  private static final MediaType JSON
+      = MediaType.parse("application/json; charset=utf-8");
+
+  public List<BusinessValue> getAll() {
+    Request request = new Request.Builder()
+        .url("http://localhost:8080/api/businessValue")
+        .addHeader("Accept", "application/json")
+        .method("GET", null)
+        .build();
+    List<BusinessValue> businessValues = new ArrayList<>();
+    try {
+      String jsonString = client.newCall(request).execute().body().string();
+      businessValues = mapper.readValue(jsonString, new TypeReference<List<BusinessValue>>() {});
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return businessValues;
+  }
+
+}

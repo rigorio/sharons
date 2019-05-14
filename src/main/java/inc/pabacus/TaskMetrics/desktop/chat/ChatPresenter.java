c2c722bac6b4113aeb5885c028f6253e77e49426
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-14 16:15:13 +0800

    Chat page

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
@@ -0,0 +1,13 @@
+package inc.pabacus.TaskMetrics.desktop.chat;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatPresenter implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}
---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatView.java
@@ -0,0 +1,6 @@
+package inc.pabacus.TaskMetrics.desktop.chat;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class ChatView extends FXMLView {
+}

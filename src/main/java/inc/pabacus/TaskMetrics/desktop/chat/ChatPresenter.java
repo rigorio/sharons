f29fcdb7263bb8443fd54adc13977919d9b0a6a1
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-15 06:46:24 +0800

    Stubs for functionality

--- src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
@@ -1,13 +1,25 @@
 package inc.pabacus.TaskMetrics.desktop.chat;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ChatPresenter implements Initializable {
+
+  @FXML
+  private JFXTextField chatBox;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+  @FXML
+  public void searchCommand() {
 
   }
 }

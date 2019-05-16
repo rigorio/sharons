4a438ef3b361d824db50c2587bfd5f7b84e51bc6
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-17 07:07:45 +0800

    Removed dirt

--- src/main/java/inc/pabacus/TaskMetrics/desktop/idle/IdlePresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/idle/IdlePresenter.java
@@ -1,13 +1,24 @@
 package inc.pabacus.TaskMetrics.desktop.idle;
 
+import com.jfoenix.controls.JFXTextArea;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IdlePresenter implements Initializable {
+  
+  @FXML
+  private JFXTextArea activity;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+  @FXML
+  public void submitActivity() {
 
   }
 }

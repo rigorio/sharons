8353a1b87be3f67bc4752983566355a5fb57a9f1
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-17 10:18:11 +0800

    Activity handler

--- src/main/java/inc/pabacus/TaskMetrics/desktop/idle/IdlePresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/idle/IdlePresenter.java
@@ -1,24 +1,47 @@
 package inc.pabacus.TaskMetrics.desktop.idle;
 
 import com.jfoenix.controls.JFXTextArea;
+import inc.pabacus.TaskMetrics.api.activity.ActivityHandler;
+import inc.pabacus.TaskMetrics.api.activity.UserActivity;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class IdlePresenter implements Initializable {
-  
+
   @FXML
-  private JFXTextArea activity;
+  private JFXTextArea activityArea;
+
+  private ActivityHandler activityHandler;
+
+  private String startTime;
+
+  private DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss", Locale.US);
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-
+    activityHandler = new ActivityHandler();
+    startTime = timeFormatter.format(LocalTime.now());
   }
 
   @FXML
   public void submitActivity() {
-
+    String activity = activityArea.getText();
+    UserActivity userActivity = UserActivity.builder()
+        .startTime(startTime)
+        .date(LocalDate.now().toString())
+        .endTime(timeFormatter.format(LocalTime.now()))
+        .activity(activity)
+        .build();
+    activityHandler.saveActivity(userActivity);
+    Stage stage = (Stage) activityArea.getScene().getWindow();
+    stage.close();
   }
 }

b3db24e32b106044cce91afeefda992652668707
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-05-21 08:18:15 +0800

    Dialog box when user trying to close the main app

--- src/main/java/inc/pabacus/TaskMetrics/TaskMetricsApplication.java
+++ src/main/java/inc/pabacus/TaskMetrics/TaskMetricsApplication.java
@@ -4,14 +4,35 @@ import inc.pabacus.TaskMetrics.api.standuply.StandupService;
 import inc.pabacus.TaskMetrics.desktop.login.LoginView;
 import inc.pabacus.TaskMetrics.utils.GuiManager;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Optional;
 
 //@SpringBootApplication
 public class TaskMetricsApplication extends Application {
 
   private static final GuiManager MANAGER = GuiManager.getInstance();
   private StandupService standupService = new StandupService();
+
+  private void preventFromClosing() {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+    stage.setAlwaysOnTop(true);
+    alert.setTitle("You're about to close the application");
+    alert.setHeaderText("Warning!");
+    alert.setContentText("You're about to close the application! \nPlease make sure there are no other window current opened or you will lost your data");
+
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.get() == ButtonType.OK){
+      Platform.exit();
+    } else {
+      alert.close();
+    }
+  }
 
   public static void main(String[] args) {
 //    SpringApplication.run(TaskMetricsApplication.class, args);
@@ -22,6 +43,11 @@ public class TaskMetricsApplication extends Application {
   public void start(Stage stage) {
     stage.setResizable(false);
     stage.getIcons().add(new Image("/img/PabacusLogo.png"));
+    //Prevent from closing
+    stage.setOnCloseRequest(evt -> {
+      evt.consume();
+      preventFromClosing();
+    });
     MANAGER.setPrimaryStage(stage);
     MANAGER.changeView(new LoginView());
   }

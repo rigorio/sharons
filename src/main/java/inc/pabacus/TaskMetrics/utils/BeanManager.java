04ee5e9b4e40a494cc5c0db9b77648bd0c2b4819
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-22 07:51:09 +0800

    Kicker service

--- src/main/java/inc/pabacus/TaskMetrics/utils/BeanManager.java
+++ src/main/java/inc/pabacus/TaskMetrics/utils/BeanManager.java
@@ -1,12 +1,18 @@
 package inc.pabacus.TaskMetrics.utils;
 
+import inc.pabacus.TaskMetrics.api.kicker.KickerService;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListener;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListenerService;
 
 public class BeanManager {
   private static ActivityListener activityListener = new ActivityListenerService();
+  private static KickerService kickerService = new KickerService();
 
   public static ActivityListener activityListener() {
     return activityListener;
+  }
+
+  public static KickerService kickerService() {
+    return kickerService;
   }
 }

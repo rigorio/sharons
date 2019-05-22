16e2d3572cebc6b1dbff9571d67c2163f95af02b
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-22 13:47:50 +0800

    Chat functionality

--- src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
@@ -1,7 +1,8 @@
 package inc.pabacus.TaskMetrics.desktop.chat;
 
 import com.jfoenix.controls.JFXTextField;
-import javafx.event.ActionEvent;
+import inc.pabacus.TaskMetrics.api.chat.ChatService;
+import inc.pabacus.TaskMetrics.utils.BeanManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -12,14 +13,16 @@ public class ChatPresenter implements Initializable {
 
   @FXML
   private JFXTextField command;
+  private ChatService chatService;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-
+    chatService = BeanManager.chatService();
   }
 
   @FXML
   public void searchCommand() {
-
+    String command = this.command.getText();
+    chatService.sendCommand(command);
   }
 }

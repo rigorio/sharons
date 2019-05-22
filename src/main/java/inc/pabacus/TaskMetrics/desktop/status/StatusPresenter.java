b182de7ca9b75fb58b14122afbe5142696515eb0
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-22 13:48:16 +0800

    Status changer

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/status/StatusPresenter.java
@@ -0,0 +1,13 @@
+package inc.pabacus.TaskMetrics.desktop.status;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StatusPresenter implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}
---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/status/StatusView.java
@@ -0,0 +1,6 @@
+package inc.pabacus.TaskMetrics.desktop.status;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class StatusView extends FXMLView {
+}

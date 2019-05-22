779dfa8487ee477089a774dc885725041118b14b
Rigo Sarmiento <RigoS@pabacus.com>
2019-05-22 13:15:11 +0800

    Chat bean

--- src/main/java/inc/pabacus/TaskMetrics/utils/BeanManager.java
+++ src/main/java/inc/pabacus/TaskMetrics/utils/BeanManager.java
@@ -1,5 +1,6 @@
 package inc.pabacus.TaskMetrics.utils;
 
+import inc.pabacus.TaskMetrics.api.chat.ChatService;
 import inc.pabacus.TaskMetrics.api.kicker.KickerService;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListener;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListenerService;
@@ -7,6 +8,7 @@ import inc.pabacus.TaskMetrics.api.listener.ActivityListenerService;
 public class BeanManager {
   private static ActivityListener activityListener = new ActivityListenerService();
   private static KickerService kickerService = new KickerService();
+  private static ChatService chatService = new ChatService();
 
   public static ActivityListener activityListener() {
     return activityListener;
@@ -14,5 +16,9 @@ public class BeanManager {
 
   public static KickerService kickerService() {
     return kickerService;
+  }
+
+  public static ChatService chatService() {
+    return chatService;
   }
 }

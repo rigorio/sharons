4ea3e1e3a9088bcc37f97730823547514007e97e
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-05-30 10:06:51 +0800

    Created Token Service and generateToken

---
+++ src/main/java/inc/pabacus/TaskMetrics/api/generateToken/TokenService.java
@@ -0,0 +1,46 @@
+package inc.pabacus.TaskMetrics.api.generateToken;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import inc.pabacus.TaskMetrics.api.standuply.StandupService;
+import inc.pabacus.TaskMetrics.api.user.UserRepository;
+import javafx.application.Platform;
+import okhttp3.*;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class TokenService {
+
+  private static final Logger logger = Logger.getLogger(StandupService.class);
+  private static final MediaType JSON
+      = MediaType.parse("application/json; charset=utf-8");
+  private static final String HOST = "http://localhost:8080";
+  private ScheduledFuture<?> scheduledFuture;
+
+  public Credentials generateToken(Credentials credentials) {
+    try {
+      OkHttpClient client = new OkHttpClient();
+      ObjectMapper mapper = new ObjectMapper();
+
+      String jsonString = mapper.writeValueAsString(credentials);
+      RequestBody body = RequestBody.create(JSON, jsonString);
+
+      Call call = client.newCall(new Request.Builder()
+                                     .url(HOST + "/token")
+                                     .post(body)
+                                     .build());
+      ResponseBody responseBody = call.execute().body();
+      String getToken = responseBody.string();
+      TokenRepository.setToken(new Token(getToken));
+      return credentials;
+    } catch (IOException e) {
+      logger.warn(e.getMessage());
+      return credentials;
+    }
+  }
+
+}

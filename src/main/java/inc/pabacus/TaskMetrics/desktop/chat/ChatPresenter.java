239d5a17b0f83411368f52d6f47939f157601baa
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-06-03 13:51:27 +0800

    Added makeFadeIn for transition

--- src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/chat/ChatPresenter.java
@@ -3,8 +3,11 @@ package inc.pabacus.TaskMetrics.desktop.chat;
 import com.jfoenix.controls.JFXTextField;
 import inc.pabacus.TaskMetrics.api.chat.ChatService;
 import inc.pabacus.TaskMetrics.utils.BeanManager;
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,11 +15,14 @@ import java.util.ResourceBundle;
 public class ChatPresenter implements Initializable {
 
   @FXML
+  private AnchorPane mainPane;
+  @FXML
   private JFXTextField command;
   private ChatService chatService;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    makeFadeIn();
     chatService = BeanManager.chatService();
   }
 
@@ -24,5 +30,14 @@ public class ChatPresenter implements Initializable {
   public void searchCommand() {
     String command = this.command.getText();
     chatService.sendCommand(command);
+  }
+
+  private void makeFadeIn(){
+    FadeTransition fadeTransition = new FadeTransition();
+    fadeTransition.setDuration(Duration.millis(1000)); // 1 second
+    fadeTransition.setNode(mainPane);
+    fadeTransition.setFromValue(0);
+    fadeTransition.setToValue(1);
+    fadeTransition.play();
   }
 }

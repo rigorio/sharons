7cd7a688e4c49427cb3166fe1f6543f32c766564
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-11 08:48:35 +0800

    Random file where you can test stuff

---
+++ src/test/java/inc/pabacus/TaskMetrics/api/Tester.java
@@ -0,0 +1,12 @@
+package inc.pabacus.TaskMetrics.api;
+
+import org.junit.Test;
+
+public class Tester {
+
+  @Test
+  public void testDoubleRoundup() {
+    double a = 300.0 / 3600.0;
+    System.out.printf("%.2f", a);
+  }
+}

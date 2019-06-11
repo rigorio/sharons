497c4faf4d1af963077976143e062ad25228dc6b
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-11 09:02:58 +0800

    Method to retrieve host

--- src/main/java/inc/pabacus/TaskMetrics/utils/Host.java
+++ src/main/java/inc/pabacus/TaskMetrics/utils/Host.java
@@ -2,4 +2,8 @@ package inc.pabacus.TaskMetrics.utils;
 
 public class Host {
   public static final String HOST = "http://localhost:5000";
+
+  public static String getHost() {
+    return HOST;
+  }
 }

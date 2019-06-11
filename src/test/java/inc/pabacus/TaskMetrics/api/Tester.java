ce005be45f719145305226f5be838fac9d3f3513
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-11 08:48:56 +0800

    @Ignore as these do not affect the application

--- src/test/java/inc/pabacus/TaskMetrics/api/Tester.java
+++ src/test/java/inc/pabacus/TaskMetrics/api/Tester.java
@@ -1,10 +1,12 @@
 package inc.pabacus.TaskMetrics.api;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class Tester {
 
   @Test
+  @Ignore
   public void testDoubleRoundup() {
     double a = 300.0 / 3600.0;
     System.out.printf("%.2f", a);

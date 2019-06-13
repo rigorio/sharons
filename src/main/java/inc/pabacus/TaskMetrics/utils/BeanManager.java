3dbb74aeba314778570dc7045fd907cf97fb696b
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-13 08:26:32 +0800

    Added hardware service API

--- src/main/java/inc/pabacus/TaskMetrics/utils/BeanManager.java
+++ src/main/java/inc/pabacus/TaskMetrics/utils/BeanManager.java
@@ -1,14 +1,18 @@
 package inc.pabacus.TaskMetrics.utils;
 
 import inc.pabacus.TaskMetrics.api.chat.ChatService;
+import inc.pabacus.TaskMetrics.api.hardware.HardwareServiceAPI;
 import inc.pabacus.TaskMetrics.api.kicker.KickerService;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListener;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListenerService;
+import inc.pabacus.TaskMetrics.api.standuply.StandupService;
 
 public class BeanManager {
   private static ActivityListener activityListener = new ActivityListenerService();
   private static KickerService kickerService = new KickerService();
   private static ChatService chatService = new ChatService();
+  private static StandupService standupService = new StandupService();
+  private static HardwareServiceAPI hardwareService = new HardwareServiceAPI();
 
   public static ActivityListener activityListener() {
     return activityListener;
@@ -20,5 +24,9 @@ public class BeanManager {
 
   public static ChatService chatService() {
     return chatService;
+  }
+
+  public static HardwareServiceAPI hardwareServiceAPI() {
+    return hardwareService;
   }
 }

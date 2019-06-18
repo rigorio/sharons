a9a89d815277371f0d39a8c793115226f36b52ed
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-18 13:43:53 +0800

    Initialized support page

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/support/SupportPresenter.java
@@ -0,0 +1,13 @@
+package inc.pabacus.TaskMetrics.desktop.support;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SupportPresenter implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}
---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/support/SupportView.java
@@ -0,0 +1,6 @@
+package inc.pabacus.TaskMetrics.desktop.support;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class SupportView extends FXMLView {
+}

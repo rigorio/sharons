02ca4a70e500559f2a0ea0646038ae4fc30c0c1c
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-18 14:17:25 +0800

    Initialized table for leave information

--- src/main/java/inc/pabacus/TaskMetrics/desktop/support/SupportPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/support/SupportPresenter.java
@@ -1,11 +1,17 @@
 package inc.pabacus.TaskMetrics.desktop.support;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SupportPresenter implements Initializable {
+
+  @FXML
+  private TableView leaveTable;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 

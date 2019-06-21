36d12871e6e6b5e46bb29e81a197e475793d3c6d
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-21 14:07:44 +0800

    Updated user activity for tracking user productivity

--- src/main/java/inc/pabacus/TaskMetrics/api/activity/UserActivity.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/activity/UserActivity.java
@@ -5,24 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity
 public class UserActivity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String date;
-  private String startTime;
-  private String endTime;
-  private String activity;
+  private String time;
+  public String activity;
+
+  public UserActivity(String dateNow, String timeNow, String activity) {
+    this.date = dateNow;
+    this.time = timeNow;
+    this.activity = activity;
+  }
 
 }

5363c9b8a1e6c5714149b559565ed78514849705
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-25 16:15:47 +0800

    Disable multiple click

--- src/main/java/inc/pabacus/TaskMetrics/desktop/tasks/TasksPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/tasks/TasksPresenter.java
@@ -110,6 +110,10 @@ public class TasksPresenter implements Initializable {
 
   @FXML
   public void startTask() {
+
+    if (TrackHandler.getSelectedTask() != null)
+      return;
+
     activityHandler.saveActivity(Activity.BUSY);
     XpmTaskAdapter selectedItem = tasksTable.getSelectionModel().getSelectedItem();
     if (selectedItem.getStatus().get().equals(Status.DONE.getStatus())) {

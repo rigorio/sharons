a3c06d05b201bab83438f152d6a77130e9ef8a96
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-26 14:51:01 +0800

    Edit task

--- src/main/java/inc/pabacus/TaskMetrics/desktop/edit/EditPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/edit/EditPresenter.java
@@ -2,108 +2,247 @@ package inc.pabacus.TaskMetrics.desktop.edit;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import inc.pabacus.TaskMetrics.api.project.ProjectHandler;
 import inc.pabacus.TaskMetrics.api.tasks.XpmTask;
 import inc.pabacus.TaskMetrics.api.tasks.XpmTaskAdapter;
 import inc.pabacus.TaskMetrics.api.tasks.XpmTaskWebHandler;
+import inc.pabacus.TaskMetrics.api.tasks.businessValue.BusinessValue;
+import inc.pabacus.TaskMetrics.api.tasks.businessValue.BusinessValueHandler;
+import inc.pabacus.TaskMetrics.api.tasks.jobTask.Job;
+import inc.pabacus.TaskMetrics.api.tasks.jobTask.JobTaskHandler;
+import inc.pabacus.TaskMetrics.api.tasks.jobTask.Task;
+import inc.pabacus.TaskMetrics.api.tasks.options.Status;
+import javafx.application.Platform;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import okhttp3.MediaType;
 
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("All")
 public class EditPresenter implements Initializable {
 
   @FXML
-  private JFXTextField titleText;
+  private JFXTextField dateCreatedField;
   @FXML
-  private JFXComboBox progressText;
+  private JFXComboBox statusComboBox;
   @FXML
-  private JFXComboBox statusText;
+  private JFXTextField startTimeField;
   @FXML
-  private JFXComboBox priorityText;
+  private Label customTaskLabel1;
   @FXML
-  private JFXTextArea descriptionText;
+  private JFXTextField endTimeField;
   @FXML
-  private JFXButton deleteTask;
+  private Label customTaskLabel2;
   @FXML
-  private JFXButton saveTask;
+  private JFXTextField totalTimeField;
+  @FXML
+  private Label customTaskLabel21;
+  @FXML
+  private Label customTaskLabel;
+  @FXML
+  private Label descriptionLabel;
+  @FXML
+  private JFXTextField customTaskField;
+  @FXML
+  private JFXTextField descriptionField;
+  @FXML
+  private JFXComboBox<String> taskCombobox;
+  @FXML
+  private JFXButton saveButton;
+  @FXML
+  private JFXButton closeButton;
+  @FXML
+  private JFXComboBox<String> jobComboBox;
 
-  private XpmTask task;
+  ObservableList<String> billableList = FXCollections.observableArrayList("True", "False");
 
-  private XpmTaskWebHandler xpmTaskWebHandler;
+  private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+  private static final String HOST = "http://localhost:8080";
+  private BusinessValueHandler businessValueHandler = new BusinessValueHandler();
+  private ProjectHandler projectHandler = new ProjectHandler();
+  private XpmTaskWebHandler xpmTaskHandler = new XpmTaskWebHandler();
+  private JobTaskHandler jobTaskHandler;
+  private XpmTaskAdapter taskBeingEdited;
+
+  public EditPresenter() {
+    jobTaskHandler = new JobTaskHandler();
+  }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    xpmTaskWebHandler = new XpmTaskWebHandler();
-    XpmTaskAdapter xpmTask = EditableTaskHolder.getTask();
-    task = new XpmTask(xpmTask);
-    initEditables();
+
+    taskBeingEdited = EditableTaskHolder.getTask();
+
+    //unfocus textfield
+    Platform.runLater(() -> closeButton.requestFocus()); // what
+
+
+    taskCombobox.setPromptText("Select a task");
+    jobComboBox.setPromptText("Select a job");
+    jobComboBox.setItems(FXCollections.observableArrayList(getJobs()));
+    customTaskField.setVisible(false);
+    customTaskLabel.setVisible(false);
+    statusComboBox.setItems(FXCollections.observableArrayList(
+        Status.IN_PROGRESS.getStatus(),
+        Status.PENDING.getStatus(),
+        Status.DONE.getStatus()
+    ));
+//    descriptionLabel.setLayoutY(203);
+//    descriptionField.setLayoutY(203);
+
+    System.out.println(taskBeingEdited);
+    StringProperty job = taskBeingEdited.getJob();
+    StringProperty task = taskBeingEdited.getTask();
+    StringProperty description = taskBeingEdited.getDescription();
+    StringProperty status = taskBeingEdited.getStatus();
+    StringProperty startTime = taskBeingEdited.getStartTime();
+    StringProperty endTime = taskBeingEdited.getEndTime();
+    StringProperty totalTimeSpent = taskBeingEdited.getTotalTimeSpent();
+    StringProperty dateCreated = taskBeingEdited.getDateCreated();
+    if (job.get() != null)
+      jobComboBox.setValue(job.get());
+    if (task.get() != null)
+      taskCombobox.setValue(task.get());
+    if (description.get() != null)
+      descriptionField.setText(description.get());
+    if (status.get() != null)
+      statusComboBox.setValue(status.get());
+    if (startTime != null)
+      startTimeField.setText(startTime.get());
+    if (endTime != null)
+      endTimeField.setText(endTime.get());
+    if (totalTimeSpent != null)
+      totalTimeField.setText(totalTimeSpent.get());
+    if (dateCreated != null)
+      dateCreatedField.setText(dateCreated.get());
+    changeTasks();
+
+
   }
 
-  public void deleteTask() {
-
-    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-    alert.setTitle("Confirmation");
-    alert.setHeaderText("Delete task?");
-    alert.setContentText("Task #" + task.getId());
-
-    Optional<ButtonType> result = alert.showAndWait();
-    if (result.isPresent() && result.get() == ButtonType.OK) {
-      xpmTaskWebHandler.deleteById(task.getId());
-      close();
-    }
+  private List<String> getJobs() {
+    return jobTaskHandler.allJobs().stream()
+        .map(Job::getJob)
+        .collect(Collectors.toList());
   }
 
-  public void saveTask() {
-    task.setTask(titleText.getText());
-    task.setDescription(descriptionText.getText());
-    xpmTaskWebHandler.save(task);
-    close();
+  @FXML
+  public void changeTasks() {
+    String project = jobComboBox.getValue();
+    Optional<Job> any = jobTaskHandler.allJobs().stream()
+        .filter(job -> job.getJob().equals(project))
+        .findAny();
+    if (!any.isPresent())
+      System.out.println("log this");
+    Job job = any.get();
+    List<Task> tasks = jobTaskHandler.allTasks();
+    List<String> filteredTasks = tasks.stream()
+        .filter(task -> task.getJobId().equals(job.getId()))
+        .map(Task::getTask)
+        .collect(Collectors.toList());
+    taskCombobox.setItems(FXCollections.observableArrayList(filteredTasks));
+    taskCombobox.getItems().add("Custom Task");
   }
 
-  private void close() {
-    EditableTaskHolder.setTask(null);
-    Stage stage = (Stage) statusText.getScene().getWindow();
+  @FXML
+  public void selectTask() {
+    String task = taskCombobox.getValue();
+    boolean status = !task.equalsIgnoreCase("Custom Task");
+    mutateFields(status);
+  }
+
+  @FXML
+  public void close() {
+    Stage stage = (Stage) jobComboBox.getScene().getWindow();
     stage.close();
   }
 
-  private void initEditables() {
+  @FXML
+  public void save() {
 
-    titleText.setText(task.getTask());
-    descriptionText.setText(task.getDescription());
+    if (isAlrightAlrightAlright() || isCustomTaskEmpty()) {
+      Alert alert = new Alert(Alert.AlertType.WARNING);
+      alert.setTitle("Error");
+      alert.setContentText("Please fill out all the fields");
+      alert.showAndWait();
+      return;
+    }
 
-    progressText.getItems().addAll(new ArrayList<String>() {{
-      add("0");
-      add("25");
-      add("50");
-      add("75");
-      add("100");
-    }});
+    String description = descriptionField.getText(); // actually task of task
+    String taskValue = taskCombobox.getValue();
+    String taskTitle = taskValue.equalsIgnoreCase("Custom Task")
+        ? customTaskField.getText()
+        : taskValue;
+//    BusinessValue businessValue = getBusinessValue();
+//    Project project = getProject();
 
-    ArrayList<String> statuses = new ArrayList<String>() {{
-      add("Pending");
-      add("In Progress");
-      add("Done");
-    }};
-    statusText.getItems().addAll(statuses);
-    statuses.add("All");
-    statusText.getItems().addAll(statuses);
+    XpmTask xpmTask = XpmTask.builder()
+        .id(taskBeingEdited.getId().get())
+        .job(jobComboBox.getValue())
+        .task(taskTitle)
+        .description(description)
+        .status(Status.convert(statusComboBox.getValue().toString()).getStatus())
+        .billable(true)
+        .totalTimeSpent(totalTimeField.getText())
+        .businessValueId(1L)
+        .dateCreated(dateCreatedField.getText())
+        .startTime(startTimeField.getText())
+        .endTime(endTimeField.getText())
+        .build();
+
+    xpmTaskHandler.save(xpmTask);
+
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setContentText("Task saved!");
+    alert.showAndWait();
+    Stage stage = (Stage) jobComboBox.getScene().getWindow();
+    stage.close();
+
+  }
 
 
-    priorityText.getItems().addAll(new ArrayList<String>() {{
-      add("1");
-      add("2");
-      add("3");
-      add("4");
-      add("5");
-    }});
+  private List<BusinessValue> getAllBusinessValues() {
+    return businessValueHandler.getAll();
+  }
+
+  private boolean isAlrightAlrightAlright() {
+    return jobComboBox.getSelectionModel().isEmpty() || taskCombobox.getSelectionModel().isEmpty() || descriptionField.getText().isEmpty();
+  }
+
+  private boolean isCustomTaskEmpty() {
+    String task = taskCombobox.getValue();
+    boolean status = task.equalsIgnoreCase("Custom Task");
+    if (status) {
+      return customTaskField.getText().isEmpty();
+    }
+    return false;
+  }
+
+  private void mutateFields(boolean status) {
+    if (status) {
+      customTaskField.setVisible(false);
+      customTaskLabel.setVisible(false);
+//      descriptionLabel.setLayoutY(203);
+//      descriptionField.setLayoutY(203);
+    } else {
+
+      customTaskField.setVisible(true);
+      customTaskLabel.setVisible(true);
+//      descriptionLabel.setLayoutY(246);
+//      descriptionField.setLayoutY(246);
+    }
   }
 
 }

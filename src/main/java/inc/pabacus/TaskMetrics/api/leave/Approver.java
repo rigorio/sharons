9e09ed6b131190aab815006a3b299a7e02f417f9
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-28 07:18:30 +0800

    Added default converter for adapter

--- src/main/java/inc/pabacus/TaskMetrics/api/leave/Approver.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/leave/Approver.java
@@ -1,14 +1,31 @@
 package inc.pabacus.TaskMetrics.api.leave;
 
+import inc.pabacus.TaskMetrics.desktop.support.ApproverAdapter;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.StringProperty;
 import lombok.Builder;
+import lombok.ToString;
 
 @Builder
+@ToString
 public class Approver {
+
   private Long approverId;
   private String approver;
   private String status;
 
   public Approver() {
+  }
+
+  public Approver(ApproverAdapter approverAdapter) {
+    LongProperty id = approverAdapter.getId();
+    this.approverId = id != null ? id.getValue() : null;
+
+    StringProperty approver = approverAdapter.getApprover();
+    this.approver = approver != null ? approver.get() : null;
+
+    StringProperty status = approverAdapter.getStatus();
+    this.status = status != null ? status.get() : null;
   }
 
   public Approver(Long approverId, String approver, String status) {

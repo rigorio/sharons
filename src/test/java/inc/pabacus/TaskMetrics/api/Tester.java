7db60e4ee16be67453109fd761cf30e383bc7cbf
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-27 09:12:43 +0800

    testing leaves

--- src/test/java/inc/pabacus/TaskMetrics/api/Tester.java
+++ src/test/java/inc/pabacus/TaskMetrics/api/Tester.java
@@ -1,9 +1,16 @@
 package inc.pabacus.TaskMetrics.api;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import inc.pabacus.TaskMetrics.api.leave.Leave;
+import inc.pabacus.TaskMetrics.api.leave.LeaveService;
+import okhttp3.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class Tester {
@@ -28,4 +35,23 @@ public class Tester {
     runnable.run();
     System.out.println("Total time is " + (System.nanoTime() - start));
   };
+
+  @Test
+  @Ignore
+  public void testingLeaves() throws IOException {
+    LeaveService leaveService = new LeaveService();
+    MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    String jsonString = new ObjectMapper().writeValueAsString(leaveService.getAll());
+    RequestBody body = RequestBody.create(JSON, jsonString);
+    Call call = new OkHttpClient()
+        .newCall(new Request.Builder()
+                     .url("http://localhost/api/user/leaves")
+                     .post(body)
+                     .build());
+
+    String responseString = call.execute().body().string();
+
+    List<Leave> leaves = new ObjectMapper().readValue(responseString, new TypeReference<List<Leave>>() {});
+    leaves.forEach(System.out::println);
+  }
 }

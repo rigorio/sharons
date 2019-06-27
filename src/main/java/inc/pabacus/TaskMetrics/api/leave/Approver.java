f79f8fa2f398a9d84c55b5582b6ab264246e5bba
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-28 07:19:23 +0800

    Transformed as lombok member

--- src/main/java/inc/pabacus/TaskMetrics/api/leave/Approver.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/leave/Approver.java
@@ -3,19 +3,20 @@ package inc.pabacus.TaskMetrics.api.leave;
 import inc.pabacus.TaskMetrics.desktop.support.ApproverAdapter;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.StringProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.ToString;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
-@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Approver {
 
   private Long approverId;
   private String approver;
   private String status;
-
-  public Approver() {
-  }
 
   public Approver(ApproverAdapter approverAdapter) {
     LongProperty id = approverAdapter.getId();
@@ -26,35 +27,5 @@ public class Approver {
 
     StringProperty status = approverAdapter.getStatus();
     this.status = status != null ? status.get() : null;
-  }
-
-  public Approver(Long approverId, String approver, String status) {
-    this.approverId = approverId;
-    this.approver = approver;
-    this.status = status;
-  }
-
-  public Long getApproverId() {
-    return approverId;
-  }
-
-  public void setApproverId(Long approverId) {
-    this.approverId = approverId;
-  }
-
-  public String getApprover() {
-    return approver;
-  }
-
-  public void setApprover(String approver) {
-    this.approver = approver;
-  }
-
-  public String getStatus() {
-    return status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
   }
 }

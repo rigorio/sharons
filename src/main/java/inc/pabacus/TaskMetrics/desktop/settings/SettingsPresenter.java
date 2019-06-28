6684cdc01e09026cc3832b6278a001c285e56cab
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-28 08:01:15 +0800

    Created listener

--- src/main/java/inc/pabacus/TaskMetrics/desktop/settings/SettingsPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/settings/SettingsPresenter.java
@@ -1,13 +1,21 @@
 package inc.pabacus.TaskMetrics.desktop.settings;
 
+import com.jfoenix.controls.JFXCheckBox;
+import inc.pabacus.TaskMetrics.desktop.tracker.AlwaysOnTopCheckerConfiguration;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SettingsPresenter implements Initializable {
+
+  @FXML
+  private JFXCheckBox alwaysOnTopCheckbox;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-
+    alwaysOnTopCheckbox.selectedProperty()
+        .addListener((observable, oldValue, newValue) -> AlwaysOnTopCheckerConfiguration.setAlwaysOnTop(newValue));
   }
 }

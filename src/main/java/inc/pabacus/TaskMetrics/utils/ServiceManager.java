579118d978deb2448a7dd3da5df9b46a792459a5
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-28 15:44:39 +0800

    Service of compiled services

---
+++ src/main/java/inc/pabacus/TaskMetrics/utils/ServiceManager.java
@@ -0,0 +1,51 @@
+package inc.pabacus.TaskMetrics.utils;
+
+import inc.pabacus.TaskMetrics.api.generateToken.TokenService;
+import inc.pabacus.TaskMetrics.api.hardware.HardwareServiceAPI;
+import inc.pabacus.TaskMetrics.api.kicker.KickerService;
+import inc.pabacus.TaskMetrics.api.kicker.TokenHolder;
+import inc.pabacus.TaskMetrics.api.listener.ActivityListener;
+import inc.pabacus.TaskMetrics.api.screenshot.ScreenshotServiceImpl;
+import inc.pabacus.TaskMetrics.api.software.SoftwareServiceAPI;
+import inc.pabacus.TaskMetrics.api.standuply.StandupService;
+import inc.pabacus.TaskMetrics.desktop.idle.IdleView;
+import javafx.application.Platform;
+
+public class ServiceManager {
+
+  private KickerService kickerService = BeanManager.kickerService();
+  private StandupService standupService = BeanManager.standupService();
+  private HardwareServiceAPI hardwareServiceAPI = BeanManager.hardwareServiceAPI();
+  private SoftwareServiceAPI softwareServiceAPI = BeanManager.softwareServiceAPI();
+  private ScreenshotServiceImpl screenshotService = BeanManager.screenshotService();
+  private TokenService tokenService = BeanManager.tokenService();
+  private ActivityListener activityListener = BeanManager.activityListener();
+
+
+  public void activate() {
+    hardwareServiceAPI.sendHardwareData();
+    standupService.runStandup();
+    softwareServiceAPI.sendSoftwareData();
+    screenshotService.enableScreenShot();
+    Runnable runnable = () -> {
+      Platform.runLater(() -> GuiManager.getInstance().displayView(new IdleView()));
+      activityListener.unListen();
+    };
+    activityListener.setEvent(runnable);
+    activityListener.setInterval(300000);
+    activityListener.listen();
+  }
+
+  public void deactivate() {
+
+    kickerService.logout(TokenHolder.getToken());
+    standupService.close();
+    hardwareServiceAPI.cancel();
+    softwareServiceAPI.cancel();
+    screenshotService.disableScreenshot();
+    screenshotService.shutdownScheduler();
+    kickerService.stopKicker();
+    tokenService.stopToken();
+    activityListener.unListen();
+  }
+}

83eb1397044897c8278b666af4522a51e3449019
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-28 09:10:45 +0800

    Productivity package

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/productivity/ProductivityPresenter.java
@@ -0,0 +1,13 @@
+package inc.pabacus.TaskMetrics.desktop.productivity;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProductivityPresenter implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}
---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/productivity/ProductivityView.java
@@ -0,0 +1,6 @@
+package inc.pabacus.TaskMetrics.desktop.productivity;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class ProductivityView extends FXMLView {
+}

2efdad27c20ef3646f3a2861bcfccb87c495c9b7
Rigo Sarmiento <RigoS@pabacus.com>
2019-06-28 13:21:14 +0800

    Get activity from combobox

--- src/main/java/inc/pabacus/TaskMetrics/desktop/idle/IdlePresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/idle/IdlePresenter.java
@@ -1,10 +1,11 @@
 package inc.pabacus.TaskMetrics.desktop.idle;
 
-import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXComboBox;
 import inc.pabacus.TaskMetrics.api.activity.ActivityHandler;
 import inc.pabacus.TaskMetrics.api.activity.UserActivity;
 import inc.pabacus.TaskMetrics.api.listener.ActivityListener;
 import inc.pabacus.TaskMetrics.utils.BeanManager;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
@@ -13,13 +14,15 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class IdlePresenter implements Initializable {
 
   @FXML
-  private JFXTextArea activityArea;
+  private JFXComboBox<String> actionsBox;
 
   private ActivityHandler activityHandler;
 
@@ -31,21 +34,37 @@ public class IdlePresenter implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     activityHandler = new ActivityHandler();
-    startTime = timeFormatter.format(LocalTime.now());
+    startTime = getTimeNow();
+
+    List<String> options = new ArrayList<>();
+    options.add("Meeting");
+    options.add("Coffee Break");
+    options.add("Bathroom Break");
+    options.add("Technical issue");
+    actionsBox.setItems(FXCollections.observableArrayList(options));
+
   }
 
   @FXML
   public void submitActivity() {
+
     ActivityListener activityListener = BeanManager.activityListener();
-//    activityListener.listen();
-    String activity = activityArea.getText();
+    activityListener.listen();
+
+
+    String activity = actionsBox.getValue();
+
     UserActivity userActivity = UserActivity.builder()
         .time(startTime)
         .date(LocalDate.now().toString())
         .activity(activity)
         .build();
     activityHandler.saveActivity(userActivity);
-    Stage stage = (Stage) activityArea.getScene().getWindow();
+    Stage stage = (Stage) actionsBox.getScene().getWindow();
     stage.close();
+  }
+
+  private String getTimeNow() {
+    return timeFormatter.format(LocalTime.now());
   }
 }

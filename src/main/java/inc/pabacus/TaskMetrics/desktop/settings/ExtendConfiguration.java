8998ac709d0c0c7e704969f0707ec2b1d2c00cdd
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-07-03 07:53:29 +0800

    Added ExtendConfiguration

---
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/settings/ExtendConfiguration.java
@@ -0,0 +1,13 @@
+package inc.pabacus.TaskMetrics.desktop.settings;
+
+public class ExtendConfiguration {
+  private static String extendMinutes;
+
+  public static String getExtendMinutes() {
+    return extendMinutes;
+  }
+
+  public static void setExtendMinutes(String extendMinutes) {
+    ExtendConfiguration.extendMinutes = extendMinutes;
+  }
+}

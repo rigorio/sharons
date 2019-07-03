a0489137817526505d06c7368fad2c7efcb4b470
Rigo Sarmiento <RigoS@pabacus.com>
2019-07-03 23:15:33 +0800

    Validation process

--- src/main/java/inc/pabacus/TaskMetrics/api/timesheet/status/ValidationService.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/timesheet/status/ValidationService.java
@@ -6,7 +6,11 @@ import inc.pabacus.TaskMetrics.api.tasks.XpmTask;
 import inc.pabacus.TaskMetrics.api.tasks.XpmTaskWebHandler;
 import inc.pabacus.TaskMetrics.utils.BeanManager;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ValidationService {
 
@@ -34,9 +38,37 @@ public class ValidationService {
   }
 
 
-  private void validation() {
+  public List<UserActivity> validation() {
+    List<UserActivity> activities = activities();
+//    System.out.println(activities);
+    List<StatusUpdate> statusUpdates = statusUpdates();
+    LocalDate today = LocalDate.now().plus(1, ChronoUnit.DAYS);
+    List<UserActivity> activitiesToday = activities.stream()
+        .filter(userActivity -> userActivity.getDate().equals(today.toString()))
+        .collect(Collectors.toList());
+//    System.out.println(activitiesToday);
+    List<StatusUpdate> updatesToday = statusUpdates.stream()
+        .filter(statusUpdate -> statusUpdate.getDate().equals(today.toString()))
+        .collect(Collectors.toList());
 
-
+    return activitiesToday.stream()
+        .filter(activity -> updatesToday.stream()
+            .anyMatch(statusUpdate -> {
+//              System.out.println("Status");
+//              System.out.println(statusUpdate);
+//              System.out.println("----");
+//              System.out.println("activity");
+//              System.out.println(activity);
+              String updateTime = statusUpdate.getTime();
+              LocalTime statusUpdateTime = LocalTime.parse(updateTime);
+              LocalTime activityTime = LocalTime.parse(activity.getTime());
+              LocalTime lowRange = statusUpdateTime.minus(3, ChronoUnit.MINUTES);
+              LocalTime highRange = statusUpdateTime.plus(3, ChronoUnit.MINUTES);
+              System.out.println(activityTime);
+              System.out.println(updateTime);
+              return (activityTime.isBefore(highRange) && activityTime.isAfter(lowRange));
+            }))
+        .collect(Collectors.toList());
   }
 
 }

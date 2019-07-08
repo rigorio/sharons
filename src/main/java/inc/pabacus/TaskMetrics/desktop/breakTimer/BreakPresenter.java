6e3d8007ff8cf36309e6fd253f7ff00818a466ef
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-07-08 08:59:50 +0800

    Removed idle during break

--- src/main/java/inc/pabacus/TaskMetrics/desktop/breakTimer/BreakPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/breakTimer/BreakPresenter.java
@@ -3,15 +3,23 @@ package inc.pabacus.TaskMetrics.desktop.breakTimer;
 import com.jfoenix.controls.JFXButton;
 import inc.pabacus.TaskMetrics.api.activity.Activity;
 import inc.pabacus.TaskMetrics.api.activity.ActivityHandler;
+import inc.pabacus.TaskMetrics.api.listener.ActivityListener;
 import inc.pabacus.TaskMetrics.api.timesheet.DailyLogHandler;
+import inc.pabacus.TaskMetrics.desktop.idle.IdleView;
+import inc.pabacus.TaskMetrics.desktop.tracker.TrackerPresenter;
 import inc.pabacus.TaskMetrics.utils.BeanManager;
+import inc.pabacus.TaskMetrics.utils.GuiManager;
 import inc.pabacus.TaskMetrics.utils.TimerService;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,8 +36,9 @@ public class BreakPresenter implements Initializable {
   private final Runnable process;
   private TimerService timerService;
   private ActivityHandler activityHandler;
+  private ActivityListener activityListener = BeanManager.activityListener();
 
-  public BreakPresenter(){
+  public BreakPresenter() {
     activityHandler = BeanManager.activityHandler();
     timerService = new TimerService();
     process = this::tickTime;
@@ -37,6 +46,8 @@ public class BreakPresenter implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    //unlisten idle
+    activityListener.unListen();
 
     Image imageView = new Image("/img/break.png");
     image.setImage(imageView);
@@ -48,24 +59,51 @@ public class BreakPresenter implements Initializable {
 
   }
 
-  private void tickTime(){
+  private void tickTime() {
+    ActivityHandler activityHandler = new ActivityHandler();
     long duration = timerService.getTime();
     String time = timerService.formatSeconds(duration);
 
     timerText.setText(time);
     //13 minutes
-    if(duration == 780){
-      timerText.setStyle("-fx-text-fill: red");
+    if (activityHandler.getLastLog().equalsIgnoreCase("break")) {
+      if (duration == 780) {
+        timerText.setStyle("-fx-text-fill: red");
+      }
+    } else if (activityHandler.getLastLog().equalsIgnoreCase("lunch")){
+      if (duration == 3300) { // 55 minutes
+        timerText.setStyle("-fx-text-fill: red");
+      }
     }
   }
 
   @FXML
-  private void backOnline(){
+  private void backOnline() {
     activityHandler.saveActivity(Activity.ONLINE);
+    notification("Online");
     timerService.pause();
     DailyLogHandler dailyLogHandler = new DailyLogHandler();
     dailyLogHandler.checkIfUserIsBreak();
+    TrackerPresenter.isContinue = true;
     Stage stage = (Stage) backOnline.getScene().getWindow();
     stage.close();
+    //reRun Listener
+    Runnable runnable = () -> {
+      Platform.runLater(() -> GuiManager.getInstance().displayView(new IdleView()));
+      activityListener.unListen();
+    };
+    activityListener.setEvent(runnable);
+    activityListener.setInterval(300000);
+    activityListener.listen();
+  }
+
+  private void notification(String notif) {
+    Notifications notifications = Notifications.create()
+        .title("TRIBELY")
+        .text("Status change to " + notif)
+        .position(Pos.BOTTOM_RIGHT)
+        .hideAfter(Duration.seconds(5));
+    notifications.darkStyle();
+    notifications.showWarning();
   }
 }

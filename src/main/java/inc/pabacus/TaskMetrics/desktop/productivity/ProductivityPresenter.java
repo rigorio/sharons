11bb2ccd110cb2da5ac64a34581ef73492fd837b
Rigo Sarmiento <RigoS@pabacus.com>
2019-07-09 11:05:37 +0800

    Update version

--- src/main/java/inc/pabacus/TaskMetrics/desktop/productivity/ProductivityPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/productivity/ProductivityPresenter.java
@@ -1,58 +1,49 @@
 package inc.pabacus.TaskMetrics.desktop.productivity;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import inc.pabacus.TaskMetrics.api.activity.ActivityHandler;
+import inc.pabacus.TaskMetrics.api.activity.UserActivity;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.*;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ProductivityPresenter implements Initializable {
 
   @FXML
-  private StackedBarChart<String, Number> stackedBarChart;
+  private AnchorPane barPane;
   @FXML
   private PieChart taskPieChart;
+
+  private ActivityHandler activityHandler;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 
-    ObservableList<PieChart.Data> pieChartData =
-        FXCollections.observableArrayList(
-            new PieChart.Data("Productive", 80),
-            new PieChart.Data("Meetings", 5),
-            new PieChart.Data("Breaks", 10),
-            new PieChart.Data("Idle", 5));
-    taskPieChart.setData(pieChartData);
-    taskPieChart.setTitle("Productivity Chart");
+    AnchorPane anchorPane = new AnchorPane();
+    anchorPane.setStyle("-fx-background-color:POWDERBLUE");
+    barPane.getChildren().add(anchorPane);
+
+    List<UserActivity> activities = activityHandler.all();
+
+    HashMap<String, Double> productivityRecords = new HashMap<>();
 
 
-    final CategoryAxis xAxis = new CategoryAxis();
-    xAxis.setLabel("Bars");
-    final NumberAxis yAxis = new NumberAxis();
-    yAxis.setLabel("Value");
+    activities.stream()
+        .filter(userActivity -> {
 
-    XYChart.Series<String, Number> series1 = new XYChart.Series<>();
-    series1.setName("1800");
-    series1.getData().add(new XYChart.Data<>("Productive", 80));
-    series1.getData().add(new XYChart.Data<>("Idle", 15));
-    series1.getData().add(new XYChart.Data<>("Productive", 57));
-    series1.getData().add(new XYChart.Data<>("Idle", 23));
-    series1.getData().add(new XYChart.Data<>("Productive", 36));
-    series1.getData().add(new XYChart.Data<>("Productive", 80));
-    series1.getData().add(new XYChart.Data<>("Productive", 52));
-    series1.getData().add(new XYChart.Data<>("Productive", 80));
-    series1.getData().add(new XYChart.Data<>("Idle", 5));
-    series1.getData().add(new XYChart.Data<>("Productive", 80));
-    series1.getData().add(new XYChart.Data<>("Productive", 52));
+          String activity = userActivity.getActivity();
+          Double duration = productivityRecords.get(activity);
+          if (duration != null)
+//            duration += userActivity.get
+            productivityRecords.containsKey(activity);
+          return true;
+        });
 
-
-//    stackedBarChart = new StackedBarChart<>(xAxis, yAxis);
-
-//    new XYChart.Series<>(xAxis, yAxis);
-    stackedBarChart.setTitle("Productivity Graph");
 
   }
 }

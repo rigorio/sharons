8c3ebffa08072c9ddb0e69f1d96e287569583051
Rigo Sarmiento <RigoS@pabacus.com>
2019-07-09 14:53:03 +0800

    Adapter

--- src/main/java/inc/pabacus/TaskMetrics/api/timesheet/time/TimeLog.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/timesheet/time/TimeLog.java
@@ -1,5 +1,7 @@
 package inc.pabacus.TaskMetrics.api.timesheet.time;
 
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.StringProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,18 +13,33 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
-@Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TimeLog {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private Long userId;
   private String date;
   private String time;
   private String status;
 
+  public TimeLog(TimeLogFXAdapter timeLog) {
+
+    LongProperty id = timeLog.getId();
+    this.id = id != null ? id.get() : null;
+
+    LongProperty userId = timeLog.getUserId();
+    this.userId = userId != null ? userId.get() : null;
+
+    StringProperty date = timeLog.getDate();
+    this.date = date != null ? date.get() : null;
+
+    StringProperty time = timeLog.getTime();
+    this.time = time != null ? time.get() : null;
+
+    StringProperty status = timeLog.getStatus();
+    this.status = status != null ? status.get() : null;
+
+  }
 }

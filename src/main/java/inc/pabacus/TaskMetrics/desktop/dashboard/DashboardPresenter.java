7bed43008dd5d5f2b7d54fb1675bf7dcf7398139
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-07-09 12:51:59 +0800

    call closeAllStages

--- src/main/java/inc/pabacus/TaskMetrics/desktop/dashboard/DashboardPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/dashboard/DashboardPresenter.java
@@ -274,8 +274,7 @@ public class DashboardPresenter implements Initializable {
       validationHandler.runValidationChecks();
       BeanManager.deactivate();
       activityHandler.saveActivity(Activity.OFFLINE);
-      GuiManager.getInstance().closeStageDisplayOnTop();
-      GuiManager.getInstance().closeStageDisplayView();
+      GuiManager.getInstance().closeAllStages(); //close opened stages
       GuiManager.getInstance().changeView(new LoginView());
     } else {
       alert.close();

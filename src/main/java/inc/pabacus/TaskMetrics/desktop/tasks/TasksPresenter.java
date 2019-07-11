47b780f92be7ac8cf93d498764f5c6360bafc15e
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-07-11 15:20:30 +0800

    Added filtering for status and time

--- src/main/java/inc/pabacus/TaskMetrics/desktop/tasks/TasksPresenter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/tasks/TasksPresenter.java
@@ -2,6 +2,7 @@ package inc.pabacus.TaskMetrics.desktop.tasks;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import inc.pabacus.TaskMetrics.api.activity.ActivityHandler;
 import inc.pabacus.TaskMetrics.api.tasks.XpmTask;
 import inc.pabacus.TaskMetrics.api.tasks.XpmTaskAdapter;
@@ -22,6 +23,8 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -33,6 +36,7 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -56,6 +60,10 @@ public class TasksPresenter implements Initializable {
   private JFXButton startButton;
   @FXML
   private TableView<XpmTaskAdapter> tasksTable;
+  @FXML
+  private JFXTextField sortTask;
+  @FXML
+  private JFXComboBox<String> timeBox;
 
   private ActivityHandler activityHandler;
   private XpmTaskWebHandler xpmTaskHandler;
@@ -71,6 +79,7 @@ public class TasksPresenter implements Initializable {
     startButton.disableProperty().bind(Bindings.isEmpty(tasksTable.getSelectionModel().getSelectedItems()));
 
     statusBox.setValue("All");
+    timeBox.setValue("All");
 
     TableColumn<XpmTaskAdapter, String> dateCreated = new TableColumn<>("Date Created");
     dateCreated.setCellValueFactory(param -> param.getValue().getDateCreated());
@@ -95,6 +104,7 @@ public class TasksPresenter implements Initializable {
                                    status, billableHours, description);
 
     initTasksTable();
+    filtering();
 
 //    initTaskSheet(); time sheet has been extracted
 //    refreshingService(); deactivated for maintenance
@@ -102,9 +112,46 @@ public class TasksPresenter implements Initializable {
       add("In Progress");
       add("Pending");
       add("Done");
-      add("Today");
       add("All");
     }});
+
+    timeBox.getItems().addAll(new ArrayList<String>() {{
+      add("Today");
+      add("Last Week");
+      add("Last Month");
+      add("Current Month");
+      add("All");
+    }});
+
+  }
+
+  private void filtering() {
+
+    sortTask.setOnKeyReleased(e -> {
+      sortTask.textProperty().addListener((observable, oldValue, newValue) -> {
+        ObservableList<XpmTaskAdapter> masterData = FXCollections.observableArrayList(getAllTasks());
+        FilteredList<XpmTaskAdapter> filteredList = new FilteredList<>(masterData, p -> true);
+        filteredList.setPredicate(sortTask -> {
+          // If filter text is empty, display all persons.
+
+          // Compare first name and last name of every person with filter text.
+          String lowerCaseFilter = newValue.toLowerCase();
+
+          if (newValue.isEmpty()) {
+            return true;
+          } else if (sortTask.getDescription().get().toLowerCase().contains(lowerCaseFilter)) {
+            return true; // Filter matches first name.
+          } else return sortTask.getTask().get().toLowerCase().contains(lowerCaseFilter); // Filter matches last name.
+          // Does not match.
+        });
+        SortedList<XpmTaskAdapter> sortedData = new SortedList<>(filteredList);
+        sortedData.comparatorProperty().bind(tasksTable.comparatorProperty());
+        tasksTable.setItems(sortedData);
+      });
+//      refreshTasks();
+
+
+    });
 
   }
 
@@ -117,9 +164,8 @@ public class TasksPresenter implements Initializable {
   @FXML
   public void updateTable() {
     String value = statusBox.getValue();
-    ObservableList<XpmTaskAdapter> tasksByStatus = value.equalsIgnoreCase("All")
-        ? FXCollections.observableArrayList(getAllTasks())
-        : getTasksByStatus(value);
+    String time = timeBox.getValue();
+    ObservableList<XpmTaskAdapter> tasksByStatus = value.equalsIgnoreCase("All") && time.equalsIgnoreCase("All") ? FXCollections.observableArrayList(getAllTasks()) : getTasksByStatus();
     tasksTable.setItems(tasksByStatus);
   }
 
@@ -168,23 +214,79 @@ public class TasksPresenter implements Initializable {
     tasksTable.setItems(FXCollections.observableArrayList(getAllTasks()));
   }
 
-  private ObservableList<XpmTaskAdapter> getTasksByStatus(String status) {
+  private ObservableList<XpmTaskAdapter> getTasksByStatus() {
+    String status = statusBox.getValue();
+    String time = timeBox.getValue();
     List<XpmTaskAdapter> backLogs;
-//    System.out.println("status " + status);
-//    System.out.println(obj);
-    if (status.equals("Today"))
-      backLogs = getAllTasks().stream()
-          .filter(backLog -> backLog.getDateCreated().get().equals(LocalDate.now().toString()))
-          .collect(Collectors.toList());
-    else
-      backLogs = getAllTasks().stream()
-          .filter(backlog -> {
-            StringProperty currentSTatus = backlog.getStatus();
-            if (currentSTatus == null)
-              currentSTatus = new SimpleStringProperty("");
-            return currentSTatus.get().equalsIgnoreCase(status);
-          })
-          .collect(Collectors.toList());
+
+    switch (time) {
+      case "Last Week":
+        backLogs = getAllTasks().stream()
+            .filter(backLog -> {
+              StringProperty currentSTatus = backLog.getStatus();
+              if (currentSTatus == null)
+                currentSTatus = new SimpleStringProperty("");
+              LocalDate date = LocalDate.now().minusDays(7);
+              LocalDate getDate = LocalDate.parse(backLog.getDateCreated().get());
+              if (status.equalsIgnoreCase("all"))
+                return getDate.isAfter(date);
+              return getDate.isAfter(date) && currentSTatus.get().equalsIgnoreCase(status);
+            })
+            .collect(Collectors.toList());
+        break;
+      case "Last Month":
+        backLogs = getAllTasks().stream()
+            .filter(backLog -> {
+              StringProperty currentSTatus = backLog.getStatus();
+              if (currentSTatus == null)
+                currentSTatus = new SimpleStringProperty("");
+              LocalDate date = LocalDate.now().minusDays(30);
+              LocalDate getDate = LocalDate.parse(backLog.getDateCreated().get());
+              if (status.equalsIgnoreCase("all"))
+                return getDate.isAfter(date);
+              return getDate.isAfter(date) && currentSTatus.get().equalsIgnoreCase(status);
+            })
+            .collect(Collectors.toList());
+        break;
+      case "Current Month":
+        backLogs = getAllTasks().stream()
+            .filter(backLog -> {
+              StringProperty currentSTatus = backLog.getStatus();
+              if (currentSTatus == null)
+                currentSTatus = new SimpleStringProperty("");
+              Month date = LocalDate.now().getMonth();
+              Month getDate = LocalDate.parse(backLog.getDateCreated().get()).getMonth();
+              if (status.equalsIgnoreCase("all"))
+                return date.equals(getDate);
+              return date.equals(getDate) && currentSTatus.get().equalsIgnoreCase(status);
+            })
+            .collect(Collectors.toList());
+        break;
+      case "Today":
+        backLogs = getAllTasks().stream()
+            .filter(backLog -> {
+              StringProperty currentSTatus = backLog.getStatus();
+              if (currentSTatus == null)
+                currentSTatus = new SimpleStringProperty("");
+              if (status.equalsIgnoreCase("all"))
+                return backLog.getDateCreated().get().equals(LocalDate.now().toString());
+              return backLog.getDateCreated().get().equals(LocalDate.now().toString()) && currentSTatus.get().equalsIgnoreCase(status);
+
+            })
+            .collect(Collectors.toList());
+        break;
+      case "All":
+      default:
+        backLogs = getAllTasks().stream()
+            .filter(backlog -> {
+              StringProperty currentSTatus = backlog.getStatus();
+              if (currentSTatus == null)
+                currentSTatus = new SimpleStringProperty("");
+              return currentSTatus.get().equalsIgnoreCase(status);
+            })
+            .collect(Collectors.toList());
+        break;
+    }
     return FXCollections.observableArrayList(backLogs);
   }
 
@@ -198,7 +300,7 @@ public class TasksPresenter implements Initializable {
     if (statusBox.getValue().equals("All"))
       initTasksTable();
     else
-      tasksTable.setItems(getTasksByStatus(statusBox.getValue()));
+      tasksTable.setItems(getTasksByStatus());
   }
 
   /**

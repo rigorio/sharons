b321531687a93fa3467f153fff9cf50af937e9a5
Rigo Sarmiento <RigoS@pabacus.com>
2019-07-18 00:09:28 +0800

    Converted from timelog to dailylog for javafx displaying

--- src/main/java/inc/pabacus/TaskMetrics/api/timesheet/time/TimeLogHandler.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/timesheet/time/TimeLogHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import inc.pabacus.TaskMetrics.api.generateToken.TokenRepository;
 import inc.pabacus.TaskMetrics.api.timesheet.DailyLogWebRepository;
+import inc.pabacus.TaskMetrics.api.timesheet.logs.DailyLog;
 import inc.pabacus.TaskMetrics.utils.HostConfig;
 import okhttp3.*;
 import org.apache.log4j.Logger;
@@ -15,6 +16,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 public class TimeLogHandler {
 
@@ -28,6 +30,27 @@ public class TimeLogHandler {
 
   public TimeLogHandler() {
     HOST = new HostConfig().getHost();
+  }
+
+  public List<DailyLog> allConvertedDailyLogs() {
+    List<TimeLog> timeLogs = all();
+    List<DailyLog> dailyLogs = new ArrayList<>();
+    for (TimeLog timeLog : timeLogs) {
+      Optional<DailyLog> anyLog = findDailyLog(dailyLogs, timeLog.getDate());
+      if (anyLog.isPresent()) {
+        DailyLog dailyLog = anyLog.get();
+        int i = dailyLogs.indexOf(dailyLog);
+        switcharoonie(dailyLog, timeLog);
+        dailyLogs.set(i, dailyLog);
+      } else {
+        DailyLog dailyLog = new DailyLog();
+        dailyLog.setDate(timeLog.getDate());
+        dailyLog.setId(timeLog.getId());
+        dailyLog.setUserId(timeLog.getUserId());
+        dailyLogs.add(switcharoonie(dailyLog, timeLog));
+      }
+    }
+    return dailyLogs;
   }
 
   public List<TimeLog> all() {
@@ -70,5 +93,31 @@ public class TimeLogHandler {
     TimeLog lastLog = all().get(all().size() - 1);
     return lastLog.getStatus();
   }
+
+  private Optional<DailyLog> findDailyLog(List<DailyLog> dailyLogs, String date) {
+    return dailyLogs.stream()
+        .filter(dailyLog -> (dailyLog.getDate() != null ? dailyLog.getDate() : "").equals(date)).findAny();
+  }
+
+  private DailyLog switcharoonie(DailyLog dailyLog, TimeLog timeLog) {
+    String status = timeLog.getStatus();
+    String time = timeLog.getTime();
+    switch (status) {
+      case "Logged In":
+        dailyLog.setIn(time);
+        break;
+      case "Lunch Break":
+        dailyLog.setOtl(time);
+        break;
+      case "Back From Lunch":
+        dailyLog.setBfl(time);
+        break;
+      case "Logged Out":
+        dailyLog.setOut(time);
+        break;
+    }
+    return dailyLog;
+  }
+
 
 }

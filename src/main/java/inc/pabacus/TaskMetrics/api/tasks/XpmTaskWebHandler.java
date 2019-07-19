ab78c53e53ba4effbe08f3e920f5e93dd92f763e
Rigo Sarmiento <RigoS@pabacus.com>
2019-07-19 20:36:45 +0800

    Dto for saving

--- src/main/java/inc/pabacus/TaskMetrics/api/tasks/XpmTaskWebHandler.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/XpmTaskWebHandler.java
@@ -3,8 +3,13 @@ package inc.pabacus.TaskMetrics.api.tasks;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import inc.pabacus.TaskMetrics.api.generateToken.TokenRepository;
+import inc.pabacus.TaskMetrics.api.generateToken.UsernameHolder;
 import inc.pabacus.TaskMetrics.utils.HostConfig;
 import inc.pabacus.TaskMetrics.utils.SslUtil;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import okhttp3.*;
 import org.apache.log4j.Logger;
 
@@ -29,7 +34,10 @@ public class XpmTaskWebHandler {
   @SuppressWarnings("all")
   public XpmTask save(XpmTask task) {
     try {
-      String jsonString = mapper.writeValueAsString(task);
+      XpmTaskDto_Save xpmDto = new XpmTaskDto_Save();
+      xpmDto.setInvoiceTypeId(1L);
+      xpmDto.setAssigneeId(1L);
+      String jsonString = mapper.writeValueAsString(xpmDto);
       RequestBody body = RequestBody.create(JSON, jsonString);
       Call call = client.newCall(new Request.Builder()
                                      .url(HOST + "/api/user/timesheet")
@@ -76,11 +84,60 @@ public class XpmTaskWebHandler {
       ResponseBody body = call.execute().body();
       String jsonString = body.string();
       tasks = mapper.readValue(jsonString, new TypeReference<List<XpmTask>>() {});
+      System.out.println(tasks);
 
     } catch (IOException e) {
       logger.warn(e.getMessage());
     }
     return tasks;
+  }
+
+  public Assignee getAssignee() {
+    List<Assignee> assignees = new ArrayList<>();
+    try {
+
+      Call call = client.newCall(new Request.Builder()
+                                     .url(HOST + "/api/jobs/assignees")
+                                     .addHeader("Authorization", TokenRepository.getToken().getToken())
+                                     .build());
+      ResponseBody body = call.execute().body();
+      String jsonString = body.string();
+      assignees = mapper.readValue(jsonString, new TypeReference<List<Assignee>>() {});
+      System.out.println(assignees);
+
+    } catch (IOException e) {
+      logger.warn(e.getMessage());
+    }
+
+    Optional<Assignee> any = assignees.stream().filter(assignee -> assignee.getUserName().equals(UsernameHolder.username))
+        .findAny();
+    Assignee assignee = any.get();
+    return assignee;
+  }
+
+  @Data
+  @Builder
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class XpmTaskDto_Save {
+    private Long clientId;
+    private Long jobId;
+    private String description;
+    private Long taskId;
+    private String status;
+    private String dateCreated;
+    private Boolean billable;
+    private String startTime;
+    private String endTime;
+    private String estimateTime;
+    private String extendCounter;
+    private String totalTimeSpent;
+    private String percentCompleted;
+    private Long businessValueId;
+    private Long invoiceTypeId;
+    private Long assigneeId;
+
+
   }
 
 }

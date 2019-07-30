814de0b5fc2cfd884b7a68734f01d48d083b228d
cmontemayor26 <46473019+cmontemayor26@users.noreply.github.com>
2019-07-30 10:04:24 +0800

    updated leaveAdapter fields

--- src/main/java/inc/pabacus/TaskMetrics/desktop/support/LeaveAdapter.java
+++ src/main/java/inc/pabacus/TaskMetrics/desktop/support/LeaveAdapter.java
@@ -1,6 +1,5 @@
 package inc.pabacus.TaskMetrics.desktop.support;
 
-import inc.pabacus.TaskMetrics.api.leave.Approver;
 import inc.pabacus.TaskMetrics.api.leave.Leave;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
@@ -11,10 +10,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Data
 @Builder
 @AllArgsConstructor
@@ -22,28 +17,30 @@ import java.util.stream.Collectors;
 public class LeaveAdapter {
 
   private LongProperty id;
-  private LongProperty userId;
-  private List<ApproverAdapter> approvers;
+  private StringProperty employeeId;
+  private StringProperty status;
+  private StringProperty reason;
+  private LongProperty amount;
   private StringProperty startDate;
   private StringProperty endDate;
-  private StringProperty reason;
-  private StringProperty status;
-  private StringProperty typeOfRequest;
+  private StringProperty leaveTypeId;
 
   public LeaveAdapter(Leave leave) {
 
     Long id = leave.getId();
     this.id = id != null ? new SimpleLongProperty(id) : null;
 
-    Long userId = leave.getUserId();
-    this.userId = userId != null ? new SimpleLongProperty(userId) : null;
+    String employeeId = leave.getEmployeeId();
+    this.employeeId = employeeId != null ? new SimpleStringProperty(employeeId) : null;
 
-    List<Approver> approver = leave.getApprovers();
-    this.approvers = approver != null
-        ? new ArrayList<>(approver.stream()
-                              .map(ApproverAdapter::new)
-                              .collect(Collectors.toList()))
-        : null;
+    String status = leave.getStatus();
+    this.status = status != null ? new SimpleStringProperty(status) : null;
+
+    String reason = leave.getReason();
+    this.reason = reason != null ? new SimpleStringProperty(reason) : null;
+
+    Long amount = leave.getAmount();
+    this.amount = amount != null ? new SimpleLongProperty(amount) : null;
 
     String startDate = leave.getStartDate();
     this.startDate = startDate != null ? new SimpleStringProperty(startDate) : null;
@@ -51,14 +48,8 @@ public class LeaveAdapter {
     String endDate = leave.getEndDate();
     this.endDate = endDate != null ? new SimpleStringProperty(endDate) : null;
 
-    String reason = leave.getReason();
-    this.reason = reason != null ? new SimpleStringProperty(reason) : null;
-
-    String status = leave.getStatus();
-    this.status = status != null ? new SimpleStringProperty(status) : null;
-
-    String typeOfRequest = leave.getTypeOfRequest();
-    this.typeOfRequest = typeOfRequest != null ? new SimpleStringProperty(typeOfRequest) : null;
+    String leaveTypeId = leave.getLeaveTypeId();
+    this.leaveTypeId = leaveTypeId != null ? new SimpleStringProperty(leaveTypeId) : null;
 
   }
 }

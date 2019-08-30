6011261c4c0eab40581839e39b4596d691ce046c
Rigo Sarmiento <RigoS@pabacus.com>
2019-08-30 09:48:26 +0800

    Logging every task action

--- src/main/java/inc/pabacus/TaskMetrics/api/tasks/XpmTaskWebHandler.java
+++ src/main/java/inc/pabacus/TaskMetrics/api/tasks/XpmTaskWebHandler.java
@@ -2,9 +2,10 @@ package inc.pabacus.TaskMetrics.api.tasks;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import inc.pabacus.TaskMetrics.api.generateToken.UsernameHolder;
 import inc.pabacus.TaskMetrics.utils.cacheService.CacheKey;
 import inc.pabacus.TaskMetrics.utils.cacheService.StringCacheService;
-import inc.pabacus.TaskMetrics.api.generateToken.UsernameHolder;
+import inc.pabacus.TaskMetrics.utils.logs.LogHelper;
 import inc.pabacus.TaskMetrics.utils.web.HostConfig;
 import inc.pabacus.TaskMetrics.utils.web.SslUtil;
 import okhttp3.*;
@@ -19,6 +20,7 @@ import java.util.Optional;
 public class XpmTaskWebHandler {
 
   private static final Logger logger = Logger.getLogger(XpmTaskWebHandler.class);
+  private LogHelper logHelper;
   private OkHttpClient client = SslUtil.getSslOkHttpClient();
   private ObjectMapper mapper = new ObjectMapper();
   private static String HOST;
@@ -28,12 +30,15 @@ public class XpmTaskWebHandler {
 
   public XpmTaskWebHandler() {
     HOST = new HostConfig().getHost();
+    logHelper = new LogHelper(logger);
+    logHelper.setClass(XpmTaskWebHandler.class);
   }
 
   @SuppressWarnings("all")
   public XpmTask save(XpmTask task) {
     try {
       XpmTaskPostEntity xpmDto = new XpmTaskPostEntity();
+      logHelper.logInfo("Saving task", xpmDto);
       String jsonString = mapper.writeValueAsString(xpmDto);
       RequestBody body = RequestBody.create(JSON, jsonString);
       Call call = client.newCall(new Request.Builder()
@@ -42,38 +47,21 @@ public class XpmTaskWebHandler {
                                      .post(body)
                                      .build());
       ResponseBody responseBody = call.execute().body();
+      String responseString = responseBody.string();
+      logHelper.logInfo("Response of save", responseString);
       XpmTask xpmTask;
-      xpmTask = mapper.readValue(responseBody.string(), new TypeReference<XpmTask>() {});
+      xpmTask = mapper.readValue(responseString, new TypeReference<XpmTask>() {});
       xpmTask.setId(xpmTask.getId());
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
     }
     return task;
   }
 
   public void save(XpmTaskPostEntity dto_save) {
     try {
+      logHelper.logInfo("Saving xpmtaskpost entity", dto_save);
       String jsonString = mapper.writeValueAsString(dto_save);
-      System.out.println(jsonString);
-      RequestBody body = RequestBody.create(JSON, jsonString);
-      Call call = client.newCall(new Request.Builder()
-                                     .url(HOST + "/api/user/timesheet")
-                                     .addHeader("Authorization", stringCacheService.get(CacheKey.TRIBELY_TOKEN))
-                                     .post(body)
-                                     .build());
-      ResponseBody responseBody = call.execute().body();
-//      XpmTask xpmTask;
-//      xpmTask = mapper.readValue(responseBody.string(), new TypeReference<XpmTask>() {});
-//      xpmTask.setId(xpmTask.getId());
-    } catch (IOException e) {
-      logger.warn(e.getMessage());
-    }
-  }
-
-  public void otherSave(TaskCreationDTO dto) {
-    try {
-      String jsonString = mapper.writeValueAsString(dto);
-      System.out.println("i wanted " + jsonString);
       RequestBody body = RequestBody.create(JSON, jsonString);
       Call call = client.newCall(new Request.Builder()
                                      .url(HOST + "/api/user/timesheet")
@@ -82,9 +70,30 @@ public class XpmTaskWebHandler {
                                      .build());
       ResponseBody responseBody = call.execute().body();
       String responseString = responseBody.string();
-      System.out.println("response string is " + responseString);
+      logHelper.logInfo("Response of saving xpmtaskpost entity", responseString);
+//      XpmTask xpmTask;
+//      xpmTask = mapper.readValue(responseBody.string(), new TypeReference<XpmTask>() {});
+//      xpmTask.setId(xpmTask.getId());
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
+    }
+  }
+
+  public void otherSave(TaskCreationDTO dto) {
+    try {
+      logHelper.logInfo("Other save for task", dto);
+      String jsonString = mapper.writeValueAsString(dto);
+      RequestBody body = RequestBody.create(JSON, jsonString);
+      Call call = client.newCall(new Request.Builder()
+                                     .url(HOST + "/api/user/timesheet")
+                                     .addHeader("Authorization", stringCacheService.get(CacheKey.TRIBELY_TOKEN))
+                                     .post(body)
+                                     .build());
+      ResponseBody responseBody = call.execute().body();
+      String responseString = responseBody.string();
+      logHelper.logInfo("Response of other save", responseString);
+    } catch (IOException e) {
+      logHelper.logError("Exception caught", e.getMessage());
     }
   }
 
@@ -103,11 +112,12 @@ public class XpmTaskWebHandler {
                                      .build());
       call.execute();
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
     }
   }
 
   public List<XpmTask> findAll() {
+    logHelper.logInfo("Retrieving all tasks", null);
     List<XpmTask> tasks = new ArrayList<>();
     try {
 
@@ -117,14 +127,16 @@ public class XpmTaskWebHandler {
                                      .build());
       ResponseBody body = call.execute().body();
       String jsonString = body.string();
+      logHelper.logInfo("Retrieved all tasks", jsonString);
       tasks = mapper.readValue(jsonString, new TypeReference<List<XpmTask>>() {});
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
     }
     return tasks;
   }
 
   public List<XpmTask> findByJobTask(Long jobTaskId) {
+    logHelper.logInfo("Find task by jobId", null);
     List<XpmTask> tasks = new ArrayList<>();
     try {
 //      System.out.println("token " + stringCacheService.get(CacheKey.TRIBELY_TOKEN));
@@ -135,8 +147,9 @@ public class XpmTaskWebHandler {
       ResponseBody body = call.execute().body();
       String jsonString = body.string();
       tasks = mapper.readValue(jsonString, new TypeReference<List<XpmTask>>() {});
+      logHelper.logInfo("Response of tasks by jobId", tasks);
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
     }
     return tasks;
   }
@@ -154,7 +167,7 @@ public class XpmTaskWebHandler {
       assignees = mapper.readValue(jsonString, new TypeReference<List<Assignee>>() {});
 
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
     }
 
     Optional<Assignee> any = assignees.stream().filter(assignee -> assignee.getUserName().equals(UsernameHolder.username))
@@ -166,9 +179,8 @@ public class XpmTaskWebHandler {
 
   public void edit(XpmTaskPostEntity helpMe) {
     try {
+      logHelper.logInfo("Editing a task", helpMe);
       String jsonString = mapper.writeValueAsString(helpMe);
-      System.out.println("editing");
-      System.out.println(jsonString);
       RequestBody body = RequestBody.create(JSON, jsonString);
       Call call = client.newCall(new Request.Builder()
                                      .url(HOST + "/api/user/timesheet/" + helpMe.getId())
@@ -178,12 +190,11 @@ public class XpmTaskWebHandler {
       ResponseBody responseBody = call.execute().body();
       XpmTask xpmTask;
       String respostring = responseBody.string();
-      System.out.println("respo");
-      System.out.println(respostring);
+      logHelper.logInfo("Response of editing a task", respostring);
       xpmTask = mapper.readValue(respostring, new TypeReference<XpmTask>() {});
       xpmTask.setId(xpmTask.getId());
     } catch (IOException e) {
-      logger.warn(e.getMessage());
+      logHelper.logError("Exception caught", e.getMessage());
     }
   }
 }

9a6cd968a8bc0aa3f9400831e1ea5e7c8a5197c8
Rigo Sarmiento <RigoS@pabacus.com>
2019-08-30 09:38:43 +0800

    Helper methods for printing logs

---
+++ src/main/java/inc/pabacus/TaskMetrics/utils/logs/LogHelper.java
@@ -0,0 +1,40 @@
+package inc.pabacus.TaskMetrics.utils.logs;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
+public class LogHelper {
+  private Logger logger;
+  private ObjectMapper mapper = new ObjectMapper();
+
+  public LogHelper(Logger logger) {
+    this.logger = logger;
+  }
+
+
+  private void logInfo(final String action, Object task) {
+    try {
+      logger.info(action + (task != null ? ": " + mapper.writeValueAsString(task) : null));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void logWarning(final String action, Object task) {
+    try {
+      logger.warn(action + (task != null ? ": " + mapper.writeValueAsString(task) : null));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void logError(final String action, Object task) {
+    try {
+      logger.error(action + (task != null ? ": " + mapper.writeValueAsString(task) : null));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+  }
+
+}

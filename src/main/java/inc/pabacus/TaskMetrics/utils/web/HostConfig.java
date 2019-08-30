6f9a368431d4effdb96da14978296704ec043b46
Rigo Sarmiento <RigoS@pabacus.com>
2019-08-30 11:22:24 +0800

    Log all host retrieval

--- src/main/java/inc/pabacus/TaskMetrics/utils/web/HostConfig.java
+++ src/main/java/inc/pabacus/TaskMetrics/utils/web/HostConfig.java
@@ -4,7 +4,9 @@ import inc.pabacus.TaskMetrics.utils.file.DefaultSettings;
 import inc.pabacus.TaskMetrics.utils.file.FlatFileRepository;
 import inc.pabacus.TaskMetrics.utils.file.FlatFileSettings;
 import inc.pabacus.TaskMetrics.utils.file.Repository;
+import inc.pabacus.TaskMetrics.utils.logs.LogHelper;
 import javafx.scene.control.TextInputDialog;
+import org.apache.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +14,8 @@ import java.util.Optional;
 
 public class HostConfig {
 
+  private static final Logger logger = Logger.getLogger(HostConfig.class);
+  private LogHelper logHelper;
   private Repository<Map<String, Object>, Long> hostRepository;
   private Repository<Map<String, Object>, Long> hrisRepository;
   private static final String KEY = "tribelyHost";
@@ -23,6 +27,7 @@ public class HostConfig {
   }
 
   public HostConfig() {
+    logHelper = new LogHelper(logger);
     FlatFileSettings flatFileSettings = new DefaultSettings();
     flatFileSettings.setFileName("tribelyHost.json"); // so we can run two tribely apps at the same time
     hostRepository = new FlatFileRepository(flatFileSettings);
@@ -46,21 +51,27 @@ public class HostConfig {
   }
 
   public String getHost() {
+    logHelper.logInfo("Retrieve tribely host ", null);
     Optional<Map<String, Object>> allList = hostRepository.findAll().stream().findAny();
+
+    logHelper.logInfo("Retrieve entire file", allList);
 
     if (!allList.isPresent()) {
       HashMap<String, Object> map = new HashMap<>();
       map.put(KEY, "");
       hostRepository.save(map);
       allList = hostRepository.findAll().stream().findAny();
+      logHelper.logInfo("Data did not exist. Writing file", allList);
     }
 
     Map<String, Object> any = allList.get();
+    logHelper.logInfo("Actual file content", any);
 
 
     Map<String, Object> sampMAp = new HashMap<>();
     String host = "";
     if (any.get(KEY).equals("") || !any.containsKey(KEY)) {
+      logHelper.logInfo("Key was not found ", null);
       TextInputDialog dialog = new TextInputDialog();
       dialog.setTitle("Error");
       dialog.setHeaderText("Host has not been set");

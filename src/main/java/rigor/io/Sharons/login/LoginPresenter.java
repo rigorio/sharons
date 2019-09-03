199bcc0ee5437c67b4dfe48b633de250c34fe32d
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-03 17:13:07 +0800

    login pages

---
+++ src/main/java/rigor/io/Sharons/login/LoginPresenter.java
@@ -0,0 +1,13 @@
+package rigor.io.Sharons.login;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginPresenter implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+}
---
+++ src/main/java/rigor/io/Sharons/login/LoginView.java
@@ -0,0 +1,6 @@
+package rigor.io.Sharons.login;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class LoginView extends FXMLView {
+}

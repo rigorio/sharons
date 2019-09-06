3abf6c8d7939a690cd8937829f0fc028cc27b78f
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-06 17:05:21 +0800

    Gown services

--- src/main/java/rigor/io/Sharons/api/GownService.java
+++ src/main/java/rigor/io/Sharons/api/GownService.java
@@ -1,4 +1,21 @@
 package rigor.io.Sharons.api;
 
-public class GownService {
+import rigor.io.Sharons.api.gown.Gown;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GownService {
+  List<Gown> all();
+
+  Optional<Gown> findById(Long id);
+
+  List<Gown> costsMoreThan(Double price);
+
+  List<Gown> costsLessThan(Double price);
+
+  List<Gown> dueOn(String date);
+
+  List<Gown> rentedOn(String date);
+
 }

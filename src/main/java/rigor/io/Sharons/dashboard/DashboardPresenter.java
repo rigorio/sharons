610d76612a61b4c0e93ae43b78c39e690aeb2172
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-06 16:48:53 +0800

    Add gown table

--- src/main/java/rigor/io/Sharons/dashboard/DashboardPresenter.java
+++ src/main/java/rigor/io/Sharons/dashboard/DashboardPresenter.java
@@ -1,11 +1,16 @@
 package rigor.io.Sharons.dashboard;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashboardPresenter implements Initializable {
+  @FXML
+  private TableView gownsTable;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 

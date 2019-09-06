07af5ad6fdc40d18e97829ff01e6f7f5675a5f37
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-06 17:00:53 +0800

    Gown list

--- src/main/java/rigor/io/Sharons/api/GownList.java
+++ src/main/java/rigor/io/Sharons/api/GownList.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class LocalCSVGownList implements GownRepository {
+public class GownList implements GownRepository {
 
   private static List<Gown> gowns = new ArrayList<>();
 

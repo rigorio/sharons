a9d4a5444e06ac4a5ca11ee19e9ade96ffd55fee
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-06 17:06:14 +0800

    Weird constructor

--- src/main/java/rigor/io/Sharons/api/gown/Gown.java
+++ src/main/java/rigor/io/Sharons/api/gown/Gown.java
@@ -13,7 +13,16 @@ public class Gown {
   private Long id;
   private String name;
   private String description;
-  private String price;
+  private Double price;
   private String dateRented;
   private String dueDate;
+
+  public Gown(Gown gown) {
+    this.id = gown.id;
+    this.name = gown.name;
+    this.description = gown.description;
+    this.price = gown.price;
+    this.dateRented = gown.dateRented;
+    this.dueDate = gown.dueDate;
+  }
 }

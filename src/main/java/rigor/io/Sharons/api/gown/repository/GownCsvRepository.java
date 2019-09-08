a3cedd39027c23d7f85a5d5e7aecbc36b1bad6a1
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-08 21:53:20 +0800

    Retrieve from csv

---
+++ src/main/java/rigor/io/Sharons/api/gown/repository/GownCsvRepository.java
@@ -0,0 +1,31 @@
+package rigor.io.Sharons.api.gown.repository;
+
+import com.univocity.parsers.common.processor.BeanListProcessor;
+import rigor.io.Sharons.api.csv.BeanUtils;
+import rigor.io.Sharons.api.gown.Gown;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GownCsvRepository {
+
+  public List<Gown> getGowns() {
+    try {
+      BeanUtils beanUtils = new BeanUtils();
+      File file = new File("");
+      InputStream inputStream = new FileInputStream(file);
+      Reader reader = beanUtils.getReader(inputStream);
+
+      BeanListProcessor<Gown> beanListProcessor = new BeanListProcessor<>(Gown.class);
+      List<Gown> gowns = beanUtils.getBeans(reader, beanListProcessor);
+
+      return gowns;
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return new ArrayList<>();
+  }
+
+}

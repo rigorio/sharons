e18c8797e0d1f6e6fd7c135550fd39582f1dd2cb
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-15 20:34:50 +0800

    gown id provider

---
+++ src/main/java/rigor/io/Sharons/utils/IdProvider.java
@@ -0,0 +1,32 @@
+package rigor.io.Sharons.utils;
+
+import rigor.io.Sharons.api.GownHandler;
+import rigor.io.Sharons.api.gown.Gown;
+import rigor.io.Sharons.api.gown.repository.GownCsvRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IdProvider {
+
+  private static Long id = 0L;
+  private static GownHandler gownService = new GownHandler(new GownCsvRepository());
+
+  public IdProvider() {
+
+  }
+
+  public static Long getId() {
+    List<Gown> all = gownService.all();
+    if (all.isEmpty())
+      return ++id;
+    else {
+      List<Gown> sortedById = all.stream()
+          .sorted((o1, o2) -> o2.getId().compareTo(o1.getId()))
+          .collect(Collectors.toList());
+      id = sortedById.get(0).getId();
+      id++;
+    }
+    return id;
+  }
+}

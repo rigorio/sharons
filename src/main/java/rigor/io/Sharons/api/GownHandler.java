c5a21574968e10ffcfe4ab52ca38e96bb619e0ac
Rigo Sarmiento <RigoS@pabacus.com>
2019-09-15 20:35:55 +0800

    Provide gown id

--- src/main/java/rigor/io/Sharons/api/GownHandler.java
+++ src/main/java/rigor/io/Sharons/api/GownHandler.java
@@ -2,6 +2,7 @@ package rigor.io.Sharons.api;
 
 import rigor.io.Sharons.api.gown.Gown;
 import rigor.io.Sharons.api.gown.repository.GownRepository;
+import rigor.io.Sharons.utils.IdProvider;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,7 @@ public class GownHandler implements GownService {
 
   @Override
   public boolean add(Gown gown) {
+    gown.setId(IdProvider.getId());
     return gownRepository.add(gown);
   }
 
